696415a477c9842403f4957214d2b8c371d169de
Tim Balsfulland <tim@balsfulland.email>
2022-12-11 14:09:16 +0100

    simplify 2022 Day 11

--- 2022/src/day11/mod.rs
+++ 2022/src/day11/mod.rs
@@ -1,6 +1,4 @@
 use itertools::Itertools;
-use std::cell::RefCell;
-use std::iter::zip;
 
 pub struct Monkey {
     items: Vec<u64>,
@@ -16,88 +14,71 @@ pub enum Operation {
     Square,
 }
 
-pub fn parse(input: &str) -> Vec<RefCell<Monkey>> {
+pub fn parse(input: &str) -> Vec<Monkey> {
     input
         .lines()
         .chunks(7)
         .into_iter()
         .map(|mut monkey| {
             monkey.next();
-            let items = monkey
-                .next()
-                .unwrap()
-                .trim_start_matches("  Starting items: ")
+
+            let items = monkey.next().unwrap()[18..]
                 .split(", ")
                 .map(|x| x.parse::<u64>().unwrap())
                 .collect();
 
-            let mut operation = monkey
-                .next()
-                .unwrap()
-                .trim_start_matches("  Operation: new = old ")
-                .split_whitespace();
-            let operator = operation.next().unwrap();
-            let value = operation.next().unwrap();
-            let operation = match [operator, value] {
-                ["*", "old"] => Operation::Square,
-                ["*", _] => Operation::Multiply(value.parse::<u64>().unwrap()),
-                ["+", _] => Operation::Add(value.parse::<u64>().unwrap()),
+            let operation = &monkey.next().unwrap()[23..];
+            let operation = match (&operation[..1], &operation[2..]) {
+                ("*", "old") => Operation::Square,
+                ("*", value) => Operation::Multiply(value.parse::<u64>().unwrap()),
+                ("+", value) => Operation::Add(value.parse::<u64>().unwrap()),
                 _ => unreachable!(),
             };
-
-            let test = monkey
-                .next()
-                .unwrap()
-                .trim_start_matches("  Test: divisible by ")
-                .parse()
-                .unwrap();
-            let if_true = monkey
-                .next()
-                .unwrap()
-                .trim_start_matches("    If true: throw to monkey ")
-                .parse()
-                .unwrap();
-            let if_false = monkey
-                .next()
-                .unwrap()
-                .trim_start_matches("    If false: throw to monkey ")
-                .parse()
-                .unwrap();
 
             Monkey {
                 items,
                 operation,
-                test,
-                if_true,
-                if_false,
+                test: monkey.next().unwrap()[21..].parse().unwrap(),
+                if_true: monkey.next().unwrap()[29..].parse().unwrap(),
+                if_false: monkey.next().unwrap()[30..].parse().unwrap(),
             }
         })
-        .map(RefCell::new)
         .collect()
 }
 
-pub fn solve_first(input: &str) -> u64 {
-    let monkeys = parse(input);
+pub fn solve<const ROUNDS: usize, const RELIEF: bool>(input: &str) -> u64 {
+    let mut monkeys = parse(input);
     let mut inspections = vec![0; monkeys.len()];
 
-    for _ in 0..20 {
-        for (monkey, inspections) in zip(monkeys.iter(), inspections.iter_mut()) {
-            let monkey = &mut *monkey.borrow_mut();
-            let mut if_true = monkeys[monkey.if_true].borrow_mut();
-            let mut if_false = monkeys[monkey.if_false].borrow_mut();
+    let all_tests = monkeys.iter().map(|x| x.test).product::<u64>();
 
-            for item in monkey.items.drain(..) {
-                *inspections += 1;
+    for _ in 0..ROUNDS {
+        for i in 0..monkeys.len() {
+            let (left, right) = monkeys.split_at_mut(i);
+            let (monkey, right) = right.split_at_mut(1);
+            let monkey = &mut monkey[0];
 
-                let item = match monkey.operation {
-                    Operation::Multiply(x) => (item * x) / 3,
-                    Operation::Add(x) => (item + x) / 3,
-                    Operation::Square => (item * item) / 3,
+            for old in monkey.items.drain(..) {
+                inspections[i] += 1;
+
+                let new = match monkey.operation {
+                    Operation::Multiply(x) => old * x,
+                    Operation::Add(x) => old + x,
+                    Operation::Square => old * old,
                 };
-                if item % monkey.test == 0 {
-                    if_true.items.push(item);
+
+                let item = if RELIEF { new / 3 } else { new % all_tests };
+
+                let next = if item % monkey.test == 0 {
+                    monkey.if_true
                 } else {
-                    if_false.items.push(item);
+                    monkey.if_false
+                };
+
+                if next < i {
+                    left[next].items.push(item);
+                } else {
+                    right[next - i - 1].items.push(item);
                 }
             }
         }
@@ -107,37 +88,12 @@ pub fn solve_first(input: &str) -> u64 {
     inspections.iter().rev().take(2).product()
 }
 
+pub fn solve_first(input: &str) -> u64 {
+    solve::<20, true>(input)
+}
+
 pub fn solve_second(input: &str) -> u64 {
-    let monkeys = parse(input);
-    let mut inspections = vec![0; monkeys.len()];
-
-    let all_tests = monkeys.iter().map(|x| x.borrow().test).product::<u64>();
-
-    for _ in 0..10000 {
-        for (monkey, inspections) in zip(monkeys.iter(), inspections.iter_mut()) {
-            let monkey = &mut *monkey.borrow_mut();
-            let mut if_true = monkeys[monkey.if_true].borrow_mut();
-            let mut if_false = monkeys[monkey.if_false].borrow_mut();
-
-            for item in monkey.items.drain(..) {
-                *inspections += 1;
-
-                let item = match monkey.operation {
-                    Operation::Multiply(x) => item * x,
-                    Operation::Add(x) => item + x,
-                    Operation::Square => item * item,
-                } % all_tests;
-                if item % monkey.test == 0 {
-                    if_true.items.push(item);
-                } else {
-                    if_false.items.push(item);
-                }
-            }
-        }
-    }
-
-    inspections.sort();
-    inspections.iter().rev().take(2).product()
+    solve::<10000, false>(input)
 }
 
 #[test]
