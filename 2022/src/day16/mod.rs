7ab53a554538bbe72b5a35e48ab6aeb6aa8d97c2
Tim Balsfulland <tim@balsfulland.email>
2022-12-16 20:46:33 +0100

    simplify 2022 Day 16

--- 2022/src/day16/mod.rs
+++ 2022/src/day16/mod.rs
@@ -2,233 +2,116 @@ use itertools::Itertools;
 use std::cmp::{max, min};
 use std::collections::HashMap;
 
-pub fn solve_first(input: &str) -> usize {
-    // PARSING
-    let valves = input
+pub fn parse(input: &str) -> Vec<(usize, Vec<usize>)> {
+    let mut indices = HashMap::new();
+    let mut start = "";
+    let mut valves = input
         .lines()
         .map(|line| {
+            if indices.is_empty() {
+                start = &line[6..8];
+            }
+            indices.insert(&line[6..8], indices.len());
             (
-                &line[6..8],
-                line.split(['=', ';'])
-                    .nth(1)
-                    .unwrap()
-                    .parse::<usize>()
-                    .unwrap(),
+                line.split(['=', ';']).nth(1).unwrap().parse().unwrap(),
                 line.split([' ', ',']).skip(9).step_by(2).collect_vec(),
             )
         })
         .collect_vec();
-    let mut indices = HashMap::new();
-    for (valve, _, _) in &valves {
-        indices.insert(*valve, indices.len());
-    }
-    let valves = valves
+
+    valves.swap(0, indices["AA"]);
+    indices.insert(start, indices["AA"]);
+    indices.insert("AA", 0);
+
+    valves
         .into_iter()
-        .map(|(valve, rate, neighbors)| {
-            (
-                indices[valve],
-                rate,
-                neighbors.into_iter().map(|n| indices[n]).collect_vec(),
-            )
-        })
-        .collect_vec();
-
-    // SOLVING
-
-    // all-pair-shortest-paths
-    let mut distances = vec![vec![usize::MAX; valves.len()]; valves.len()];
-    for (valve, _, neighbors) in &valves {
-        distances[*valve][*valve] = 0;
-        for neighbor in neighbors {
-            distances[*valve][*neighbor] = 1;
-        }
-    }
-    for k in 0..valves.len() {
-        for i in 0..valves.len() {
-            for j in 0..valves.len() {
-                distances[i][j] = min(
-                    distances[i][j],
-                    usize::saturating_add(distances[i][k], distances[k][j]),
-                );
-            }
-        }
-    }
-
-    // only need to look at valves with rate>0 and the start valve
-    // TODO reduce valves and re-index
-
-    // brute-force solution
-    fn find_best(
-        current: usize,
-        mut visited: u64,
-        remaining: usize,
-        valves: &Vec<(usize, usize, Vec<usize>)>,
-        distances: &Vec<Vec<usize>>,
-    ) -> usize {
-        if remaining == 0 {
-            return 0;
-        }
-
-        visited |= 1 << current;
-        (0..64)
-            .map(|i| visited & (1 << i) != 0)
-            .enumerate()
-            .filter(|(_, x)| !*x)
-            .map(|(target, _)| {
-                find_best(
-                    target,
-                    visited,
-                    remaining
-                        .saturating_sub(distances[current][target])
-                        .saturating_sub(1),
-                    valves,
-                    distances,
-                )
-            })
-            .max()
-            .unwrap_or_default()
-            + remaining * valves[current].1
-    }
-
-    let mut visited = u64::MAX;
-    for (valve, rate, _) in &valves {
-        if *rate != 0 {
-            visited ^= 1 << valve;
-        }
-    }
-
-    find_best(indices["AA"], visited, 30, &valves, &distances)
+        .map(|(rate, neighbors)| (rate, neighbors.into_iter().map(|n| indices[n]).collect()))
+        .collect()
 }
 
-pub fn solve_second(input: &str) -> usize {
-    // PARSING
-    let valves = input
-        .lines()
-        .map(|line| {
-            (
-                &line[6..8],
-                line.split(['=', ';'])
-                    .nth(1)
-                    .unwrap()
-                    .parse::<usize>()
-                    .unwrap(),
-                line.split([' ', ',']).skip(9).step_by(2).collect_vec(),
-            )
-        })
-        .collect_vec();
-    let mut indices = HashMap::new();
-    for (valve, _, _) in &valves {
-        indices.insert(*valve, indices.len());
-    }
-    let valves = valves
-        .into_iter()
-        .map(|(valve, rate, neighbors)| {
-            (
-                indices[valve],
-                rate,
-                neighbors.into_iter().map(|n| indices[n]).collect_vec(),
-            )
-        })
-        .collect_vec();
-
-    // SOLVING
-
-    // all-pair-shortest-paths
-    let mut distances = vec![vec![usize::MAX; valves.len()]; valves.len()];
-    for (valve, _, neighbors) in &valves {
-        distances[*valve][*valve] = 0;
-        for neighbor in neighbors {
-            distances[*valve][*neighbor] = 1;
+pub fn all_pairs_shortest_paths(valves: &Vec<(usize, Vec<usize>)>) -> Vec<Vec<usize>> {
+    let mut distances = vec![vec![usize::MAX / 2; valves.len()]; valves.len()];
+    for (valve, (_, neighbors)) in valves.iter().enumerate() {
+        distances[valve][valve] = 0;
+        for &neighbor in neighbors {
+            distances[valve][neighbor] = 1;
         }
     }
     for k in 0..valves.len() {
         for i in 0..valves.len() {
             for j in 0..valves.len() {
-                distances[i][j] = min(
-                    distances[i][j],
-                    usize::saturating_add(distances[i][k], distances[k][j]),
-                );
+                distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
             }
         }
     }
+    distances
+}
 
-    // only need to look at valves with rate>0 and the start valve
-    // TODO reduce valves and re-index
+pub fn solve(input: &str, time: usize, actors: usize) -> usize {
+    let valves = parse(input);
+    let distances = all_pairs_shortest_paths(&valves);
 
-    // brute-force solution
-    fn find_best(
-        start: usize,
+    fn search(
         current: usize,
-        elephant: bool,
-        mut visited: u64,
+        actors: usize,
+        visited: usize,
         remaining: usize,
-        valves: &Vec<(usize, usize, Vec<usize>)>,
+        valves: &Vec<(usize, Vec<usize>)>,
         distances: &Vec<Vec<usize>>,
+        time: usize,
     ) -> usize {
         if remaining == 0 {
             return 0;
         }
 
-        let score = remaining * valves[current].1;
+        let visited = visited | (1 << current);
 
-        visited |= 1 << current;
-        let score_if_keep_going = (0..64)
-            .map(|i| visited & (1 << i) != 0)
-            .enumerate()
-            .filter(|(_, x)| !*x)
-            .map(|(target, _)| {
-                find_best(
-                    start,
+        let score_if_keep_going = (0..valves.len())
+            .filter(|&i| visited & (1 << i) == 0)
+            .map(|target| {
+                search(
                     target,
-                    elephant,
+                    actors,
                     visited,
-                    remaining
-                        .saturating_sub(distances[current][target])
-                        .saturating_sub(1),
+                    remaining.saturating_sub(distances[current][target] + 1),
                     valves,
                     distances,
+                    time,
                 )
             })
             .max()
             .unwrap_or_default();
 
-        let score_if_stop = if !elephant {
-            find_best(start, start, true, visited, 26, valves, distances)
+        let score_if_stop = if actors > 1 {
+            search(0, actors - 1, visited, time, valves, distances, time)
         } else {
             0
         };
 
-        max(score_if_keep_going, score_if_stop) + score
+        max(score_if_keep_going, score_if_stop) + remaining * valves[current].0
     }
 
-    let mut visited = u64::MAX;
-    for (valve, rate, _) in &valves {
+    let mut visited = usize::MAX;
+    for (valve, (rate, _)) in valves.iter().enumerate() {
+        // can ignore valves with rate==0
         if *rate != 0 {
             visited ^= 1 << valve;
         }
     }
 
-    find_best(
-        indices["AA"],
-        indices["AA"],
-        false,
-        visited,
-        26,
-        &valves,
-        &distances,
-    )
+    search(0, actors, visited, time, &valves, &distances, time)
 }
 
 #[test]
 pub fn sample() {
     let sample = include_str!("sample.txt");
-    assert_eq!(1651, solve_first(sample));
-    assert_eq!(1707, solve_second(sample));
+    assert_eq!(1651, solve(sample, 30, 1));
+    assert_eq!(1707, solve(sample, 26, 2));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(1617, solve_first(input));
-    assert_eq!(2171, solve_second(input));
+    assert_eq!(1617, solve(input, 30, 1));
+    assert_eq!(2171, solve(input, 26, 2));
 }
