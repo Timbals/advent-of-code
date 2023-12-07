e0aa377d0ae214f5aa8d0bf0273d71ff935c3982
Tim Balsfulland <tim@balsfulland.email>
2023-12-07 14:22:43 +0100

    simplify 2023 Day 7

--- 2023/src/day07/mod.rs
+++ 2023/src/day07/mod.rs
@@ -3,29 +3,51 @@ use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::iter::zip;
 
-fn hand_type(hand: &str) -> usize {
-    let mut counts = HashMap::<char, usize>::new();
-    for c in hand.chars() {
-        *counts.entry(c).or_default() += 1;
-    }
-    let counts = counts.into_values().sorted().collect::<Vec<_>>();
-
-    match &counts.as_slice() {
-        [.., 5] => 6,
-        [.., 4] => 5,
-        [.., 2, 3] => 4,
-        [.., 3] => 3,
-        [.., 2, 2] => 2,
-        [.., 2] => 1,
-        _ => 0,
+fn card_value(card: char, joker: bool) -> usize {
+    match card {
+        'J' if joker => 1,
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'T' => 10,
+        'J' if !joker => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => unreachable!(),
     }
 }
 
-static CARDS: [char; 13] = [
-    '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
-];
+fn hand_type(hand: &str, joker: bool) -> usize {
+    let mut counts = HashMap::<char, usize>::new();
+    for card in hand.chars() {
+        *counts.entry(card).or_default() += 1;
+    }
 
-pub fn solve_first(input: &str) -> usize {
+    let joker_count = if joker {
+        counts.remove(&'J').unwrap_or(0)
+    } else {
+        0
+    };
+
+    match &counts.into_values().sorted().collect::<Vec<_>>().as_slice() {
+        [.., x] if x + joker_count >= 5 => 6,        // five of a kind
+        [..] if joker_count >= 5 => 6,               // five of a kind
+        [.., x] if x + joker_count >= 4 => 5,        // four of a kind
+        [.., x, y] if x + y + joker_count >= 5 => 4, // full house
+        [.., x] if x + joker_count >= 3 => 3,        // three of a kind
+        [.., x, y] if x + y + joker_count >= 4 => 2, // two pair
+        [.., x] if x + joker_count >= 2 => 1,        // one pair
+        _ => 0,                                      // high card
+    }
+}
+
+pub fn solve(input: &str, joker: bool) -> usize {
     input
         .lines()
         .map(|line| {
@@ -34,88 +56,15 @@ pub fn solve_first(input: &str) -> usize {
 
             (hand, bid)
         })
-        .sorted_by(|(a, _), (b, _)| match hand_type(a).cmp(&hand_type(b)) {
-            Ordering::Equal => {
-                for (a, b) in zip(a.chars(), b.chars()) {
-                    let cmp = CARDS
-                        .iter()
-                        .position(|&x| x == a)
-                        .unwrap()
-                        .cmp(&CARDS.iter().position(|&x| x == b).unwrap());
-                    if cmp != Ordering::Equal {
-                        return cmp;
-                    }
-                }
-                Ordering::Equal
-            }
-            ord => ord,
-        })
-        .enumerate()
-        .map(|(i, (_, bid))| (i + 1) * bid)
-        .sum()
-}
-
-fn hand_type_2(hand: &str) -> usize {
-    let mut counts = HashMap::<char, usize>::new();
-    for c in hand.chars() {
-        *counts.entry(c).or_default() += 1;
-    }
-    let joker_count = counts.remove(&'J').unwrap_or(0);
-
-    let counts = counts.into_values().sorted().collect::<Vec<_>>();
-
-    match &counts.as_slice() {
-        [.., x] if x + joker_count >= 5 => 6,
-        [..] if joker_count >= 5 => 6,
-
-        [.., x] if x + joker_count >= 4 => 5,
-
-        [.., 2, 3] => 4,
-        [.., 1, 3] | [.., 2, 2] if joker_count >= 1 => 4,
-        [.., 1, 2] | [.., 3] if joker_count >= 2 => 4,
-        [.., 1, 1] | [.., 2] if joker_count >= 3 => 4,
-
-        [.., x] if x + joker_count >= 3 => 3,
-
-        [.., 2, 2] => 2,
-        [.., 1, 2] if joker_count >= 1 => 2,
-        [.., 1, 1] | [.., 2] if joker_count >= 2 => 2,
-
-        [.., x] if x + joker_count >= 2 => 1,
-
-        _ => 0,
-    }
-}
-
-static CARDS_2: [char; 13] = [
-    'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
-];
-
-pub fn solve_second(input: &str) -> usize {
-    input
-        .lines()
-        .map(|line| {
-            let (hand, bid) = line.split_once(' ').unwrap();
-            let bid = bid.parse::<usize>().unwrap();
-
-            (hand, bid)
-        })
-        .sorted_by(|(a, _), (b, _)| match hand_type_2(a).cmp(&hand_type_2(b)) {
-            Ordering::Equal => {
-                for (a, b) in zip(a.chars(), b.chars()) {
-                    let cmp = CARDS_2
-                        .iter()
-                        .position(|&x| x == a)
-                        .unwrap()
-                        .cmp(&CARDS_2.iter().position(|&x| x == b).unwrap());
-                    if cmp != Ordering::Equal {
-                        return cmp;
-                    }
-                }
-                Ordering::Equal
-            }
-            ord => ord,
-        })
+        .sorted_unstable_by(
+            |(a, _), (b, _)| match hand_type(a, joker).cmp(&hand_type(b, joker)) {
+                Ordering::Equal => zip(a.chars(), b.chars())
+                    .find(|(a, b)| a != b)
+                    .map(|(a, b)| card_value(a, joker).cmp(&card_value(b, joker)))
+                    .unwrap_or(Ordering::Equal),
+                ord => ord,
+            },
+        )
         .enumerate()
         .map(|(i, (_, bid))| (i + 1) * bid)
         .sum()
@@ -124,13 +73,13 @@ pub fn solve_second(input: &str) -> usize {
 #[test]
 pub fn sample() {
     let sample = include_str!("sample.txt");
-    assert_eq!(6440, solve_first(sample));
-    assert_eq!(5905, solve_second(sample));
+    assert_eq!(6440, solve(sample, false));
+    assert_eq!(5905, solve(sample, true));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(251058093, solve_first(input));
-    assert_eq!(249781879, solve_second(input));
+    assert_eq!(251058093, solve(input, false));
+    assert_eq!(249781879, solve(input, true));
 }
