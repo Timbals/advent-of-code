4a11d2807db8398ba26130a349fed6b7b2a374f2
Tim Balsfulland <tim@balsfulland.email>
2023-12-09 17:17:53 +0100

    simplify 2023 Day 9 further
    - don't need to store the final values
    - can reverse the list after parsing for part two

--- 2023/src/day09/mod.rs
+++ 2023/src/day09/mod.rs
@@ -1,20 +1,17 @@
 use itertools::Itertools;
-use std::collections::VecDeque;
-use std::ops::{Add, Sub};
+use std::ops::Add;
 
-fn solve(
-    input: &str,
-    push: fn(&mut VecDeque<isize>, isize),
-    access: fn(&VecDeque<isize>) -> Option<&isize>,
-    combine: fn(isize, isize) -> isize,
-) -> isize {
+pub fn solve(input: &str, reverse: bool) -> isize {
     input
         .lines()
         .map(|line| {
-            let sequence = line
-                .split_whitespace()
-                .map(|x| x.parse::<isize>().unwrap())
-                .collect::<VecDeque<_>>();
+            let sequence = line.split_whitespace().map(|x| x.parse::<isize>().unwrap());
+
+            let sequence: Vec<_> = if reverse {
+                sequence.rev().collect()
+            } else {
+                sequence.collect()
+            };
 
             let mut sequences = vec![sequence];
             while !sequences.last().unwrap().iter().all(|&x| x == 0) {
@@ -29,38 +26,25 @@ fn solve(
                 );
             }
 
-            push(sequences.last_mut().unwrap(), 0);
-            for i in (1..sequences.len()).rev() {
-                let next_value = combine(
-                    *access(&sequences[i - 1]).unwrap(),
-                    *access(&sequences[i]).unwrap(),
-                );
-                push(&mut sequences[i - 1], next_value);
-            }
-
-            *access(sequences.first().unwrap()).unwrap()
+            sequences
+                .into_iter()
+                .map(|x| *x.last().unwrap())
+                .reduce(isize::add)
+                .unwrap()
         })
         .sum()
-}
-
-pub fn solve_first(input: &str) -> isize {
-    solve(input, VecDeque::push_back, VecDeque::back, isize::add)
-}
-
-pub fn solve_second(input: &str) -> isize {
-    solve(input, VecDeque::push_front, VecDeque::front, isize::sub)
 }
 
 #[test]
 pub fn sample() {
     let sample = include_str!("sample.txt");
-    assert_eq!(114, solve_first(sample));
-    assert_eq!(2, solve_second(sample));
+    assert_eq!(114, solve(sample, false));
+    assert_eq!(2, solve(sample, true));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(1762065988, solve_first(input));
-    assert_eq!(1066, solve_second(input));
+    assert_eq!(1762065988, solve(input, false));
+    assert_eq!(1066, solve(input, true));
 }
