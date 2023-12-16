ab77dfb4a9f93ac766d8d02b6012d9cd449bac51
Tim Balsfulland <tim@balsfulland.email>
2023-12-16 20:01:40 +0100

    simplify 2023 Day 16

--- 2023/src/day16/mod.rs
+++ 2023/src/day16/mod.rs
@@ -1,128 +1,82 @@
-pub fn solve_first(input: &str) -> usize {
-    let mut grid = input
-        .lines()
-        .map(|line| line.chars().map(|c| (c, [false; 4])).collect::<Vec<_>>())
+use itertools::chain;
+use std::iter::once;
+
+fn parse(input: &str) -> Vec<Vec<char>> {
+    input.lines().map(|line| line.chars().collect()).collect()
+}
+
+const RIGHT: usize = 0;
+const DOWN: usize = 1;
+const LEFT: usize = 2;
+const UP: usize = 3;
+
+fn solve(grid: &[Vec<char>], start: (usize, usize, usize)) -> usize {
+    let mut energy = grid
+        .iter()
+        .map(|row| row.iter().map(|_| [false; 4]).collect::<Vec<_>>())
         .collect::<Vec<_>>();
-    // (x, y, direction), right = 0; down = 1; left = 2; up = 3
-    let mut queue = vec![(0, 0, 0)];
 
-    while let Some((x, y, dir)) = queue.pop() {
-        if y >= grid.len() || x >= grid[0].len() {
-            continue;
-        }
+    let mut stack = vec![start];
 
-        if grid[y][x].1[dir] {
-            continue;
-        }
+    while let Some((x, y, dir)) = stack.pop() {
+        energy[y][x][dir] = true;
 
-        grid[y][x].1[dir] = true;
-
-        let mut new_dirs = Vec::with_capacity(2);
-
-        match grid[y][x].0 {
-            '.' => new_dirs.push(dir),
-            '/' if dir == 0 || dir == 3 => new_dirs.push((dir + 3) % 6),
-            '/' if dir == 1 || dir == 2 => new_dirs.push((dir % 2) + 1),
-            '\\' if dir == 0 || dir == 1 => new_dirs.push((dir + 1) % 2),
-            '\\' if dir == 2 => new_dirs.push(3),
-            '\\' if dir == 3 => new_dirs.push(2),
-            '|' if dir == 1 || dir == 3 => new_dirs.push(dir),
-            '|' if dir == 0 || dir == 2 => new_dirs.extend_from_slice(&[1, 3]),
-            '-' if dir == 0 || dir == 2 => new_dirs.push(dir),
-            '-' if dir == 1 || dir == 3 => new_dirs.extend_from_slice(&[0, 2]),
+        let (new_dir1, new_dir2) = match grid[y][x] {
+            '.' => (dir, None),
+            '/' if dir == RIGHT => (UP, None),
+            '/' if dir == UP => (RIGHT, None),
+            '/' if dir == DOWN => (LEFT, None),
+            '/' if dir == LEFT => (DOWN, None),
+            '\\' if dir == RIGHT => (DOWN, None),
+            '\\' if dir == DOWN => (RIGHT, None),
+            '\\' if dir == LEFT => (UP, None),
+            '\\' if dir == UP => (LEFT, None),
+            '|' if dir == DOWN || dir == UP => (dir, None),
+            '|' if dir == RIGHT || dir == LEFT => (DOWN, Some(UP)),
+            '-' if dir == RIGHT || dir == LEFT => (dir, None),
+            '-' if dir == DOWN || dir == UP => (RIGHT, Some(LEFT)),
             _ => unreachable!(),
-        }
+        };
 
-        for new_dir in new_dirs {
-            match new_dir {
-                0 => queue.push((x + 1, y, new_dir)),
-                1 => queue.push((x, y + 1, new_dir)),
-                2 => queue.push((x.wrapping_sub(1), y, new_dir)),
-                3 => queue.push((x, y.wrapping_sub(1), new_dir)),
-                _ => unreachable!(),
+        for new_dir in once(new_dir1).chain(new_dir2) {
+            let (new_x, new_y) = match new_dir {
+                RIGHT if x + 1 < grid[0].len() => (x + 1, y),
+                DOWN if y + 1 < grid.len() => (x, y + 1),
+                LEFT if x > 0 => (x - 1, y),
+                UP if y > 0 => (x, y - 1),
+                _ => continue, // out of bounds
+            };
+
+            // check if already energized in the direction
+            if !energy[new_y][new_x][new_dir] {
+                stack.push((new_x, new_y, new_dir))
             }
         }
     }
 
-    grid.iter()
-        .flat_map(|row| row.iter().filter(|(_, x)| x.iter().any(|&y| y)))
+    energy
+        .iter()
+        .flat_map(|row| row.iter().filter(|x| x.iter().any(|&y| y)))
         .count()
 }
 
+pub fn solve_first(input: &str) -> usize {
+    let grid = parse(input);
+    solve(&grid, (0, 0, 0))
+}
+
 pub fn solve_second(input: &str) -> usize {
-    let grid = input
-        .lines()
-        .map(|line| line.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+    let grid = parse(input);
 
-    let mut starts = Vec::new();
-
-    for y in 0..grid.len() {
-        starts.push((0, y, 0));
-        starts.push((grid[0].len() - 1, y, 2));
-    }
-
-    for x in 0..grid[0].len() {
-        starts.push((x, 0, 1));
-        starts.push((x, grid.len() - 1, 3));
-    }
-
-    starts
-        .into_iter()
-        .map(|start| {
-            let mut energy = grid
-                .iter()
-                .map(|row| row.iter().map(|_| [false; 4]).collect::<Vec<_>>())
-                .collect::<Vec<_>>();
-
-            // (x, y, direction), right = 0; down = 1; left = 2; up = 3
-            let mut queue = vec![start];
-
-            while let Some((x, y, dir)) = queue.pop() {
-                if y >= grid.len() || x >= grid[0].len() {
-                    continue;
-                }
-
-                if energy[y][x][dir] {
-                    continue;
-                }
-
-                energy[y][x][dir] = true;
-
-                let mut new_dirs = Vec::with_capacity(2);
-
-                match grid[y][x] {
-                    '.' => new_dirs.push(dir),
-                    '/' if dir == 0 || dir == 3 => new_dirs.push((dir + 3) % 6),
-                    '/' if dir == 1 || dir == 2 => new_dirs.push((dir % 2) + 1),
-                    '\\' if dir == 0 || dir == 1 => new_dirs.push((dir + 1) % 2),
-                    '\\' if dir == 2 => new_dirs.push(3),
-                    '\\' if dir == 3 => new_dirs.push(2),
-                    '|' if dir == 1 || dir == 3 => new_dirs.push(dir),
-                    '|' if dir == 0 || dir == 2 => new_dirs.extend_from_slice(&[1, 3]),
-                    '-' if dir == 0 || dir == 2 => new_dirs.push(dir),
-                    '-' if dir == 1 || dir == 3 => new_dirs.extend_from_slice(&[0, 2]),
-                    _ => unreachable!(),
-                }
-
-                for new_dir in new_dirs {
-                    match new_dir {
-                        0 => queue.push((x + 1, y, new_dir)),
-                        1 => queue.push((x, y + 1, new_dir)),
-                        2 => queue.push((x.wrapping_sub(1), y, new_dir)),
-                        3 => queue.push((x, y.wrapping_sub(1), new_dir)),
-                        _ => unreachable!(),
-                    }
-                }
-            }
-
-            dbg!(energy
-                .iter()
-                .flat_map(|row| row.iter().filter(|x| x.iter().any(|&y| y)))
-                .count())
-        })
-        .max()
-        .unwrap()
+    chain!(
+        (0..grid.len()).map(|y| (0, y, 0)),                 // left side
+        (0..grid[0].len()).map(|x| (x, 0, 1)),              // top side
+        (0..grid.len()).map(|y| (grid[0].len() - 1, y, 2)), // right side
+        (0..grid[0].len()).map(|x| (x, grid.len() - 1, 3)), // bottom side
+    )
+    .map(|start| solve(&grid, start))
+    .max()
+    .unwrap()
 }
 
 #[test]
