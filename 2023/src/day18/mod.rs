37a4a67948252f5732adc8c76d5ad7803cbc0dab
Tim Balsfulland <tim@balsfulland.email>
2023-12-18 15:55:23 +0100

    simplify 2023 Day 18

--- 2023/src/day18/mod.rs
+++ 2023/src/day18/mod.rs
@@ -1,92 +1,58 @@
 use itertools::Itertools;
 
-pub fn solve_first(input: &str) -> usize {
-    let mut b = 0;
+pub fn solve(input: &str, use_color: bool) -> usize {
+    let mut edge_points = 0;
 
-    let a = input
+    let area = input
         .lines()
         .map(|line| {
-            let (dir, tail) = line.split_once(' ').unwrap();
-            let (distance, _) = tail.split_once(' ').unwrap();
-            let dir = dir.chars().next().unwrap();
-            let distance = distance.parse::<isize>().unwrap();
-            (dir, distance)
+            let (direction, tail) = line.split_once(' ').unwrap();
+            let (distance, color) = tail.split_once(' ').unwrap();
+
+            if use_color {
+                (
+                    match color.as_bytes()[7] {
+                        b'0' => b'R',
+                        b'1' => b'D',
+                        b'2' => b'L',
+                        b'3' => b'U',
+                        _ => unreachable!(),
+                    },
+                    isize::from_str_radix(&color[2..7], 16).unwrap(),
+                )
+            } else {
+                (direction.as_bytes()[0], distance.parse::<isize>().unwrap())
+            }
         })
-        .scan((0, 0), |current, (dir, distance)| {
-            let (dx, dy) = match dir {
-                'R' => (distance, 0),
-                'D' => (0, distance),
-                'L' => (-distance, 0),
-                'U' => (0, -distance),
+        .inspect(|(_, distance)| edge_points += distance)
+        .scan((0, 0), |current, (direction, distance)| {
+            let (dx, dy) = match direction {
+                b'R' => (distance, 0),
+                b'D' => (0, distance),
+                b'L' => (-distance, 0),
+                b'U' => (0, -distance),
                 _ => unreachable!(),
             };
 
-            b += distance;
-
             *current = (current.0 + dx, current.1 + dy);
-
             Some(*current)
         })
-        .collect::<Vec<_>>()
+        .collect::<Vec<_>>() // unfortunate, needed because `circular_tuple_windows` has an `ExactSizeIterator` constraint
         .into_iter()
         .circular_tuple_windows()
         .map(|((x1, y1), (x2, y2))| (y1 + y2) * (x1 - x2)) // shoelace formula
-        .sum::<isize>() as usize
+        .sum::<isize>()
         / 2;
-    let b = b as usize;
 
-    a + 1 - b / 2 + b // pick's theorem
+    (area + 1 - edge_points / 2 + edge_points) as usize // pick's theorem
+}
+
+pub fn solve_first(input: &str) -> usize {
+    solve(input, false)
 }
 
 pub fn solve_second(input: &str) -> usize {
-    let mut b = 0;
-
-    let a = input
-        .lines()
-        .map(|line| {
-            let (_, tail) = line.split_once(' ').unwrap();
-            let (_, color) = tail.split_once(' ').unwrap();
-
-            let mut distance = 0;
-            for (i, digit) in color[2..7].chars().rev().enumerate() {
-                distance += 16_u32.pow(i as _) * digit.to_digit(16).unwrap();
-            }
-            let distance = distance as isize;
-
-            let dir = match color.chars().nth(7).unwrap() {
-                '0' => 'R',
-                '1' => 'D',
-                '2' => 'L',
-                '3' => 'U',
-                _ => unreachable!(),
-            };
-
-            (dir, distance)
-        })
-        .scan((0, 0), |current, (dir, distance)| {
-            let (dx, dy) = match dir {
-                'R' => (distance, 0),
-                'D' => (0, distance),
-                'L' => (-distance, 0),
-                'U' => (0, -distance),
-                _ => unreachable!(),
-            };
-
-            b += distance;
-
-            *current = (current.0 + dx, current.1 + dy);
-
-            Some(*current)
-        })
-        .collect::<Vec<_>>()
-        .into_iter()
-        .circular_tuple_windows()
-        .map(|((x1, y1), (x2, y2))| (y1 + y2) * (x1 - x2)) // shoelace formula
-        .sum::<isize>() as usize
-        / 2;
-    let b = b as usize;
-
-    a + 1 - b / 2 + b // pick's theorem
+    solve(input, true)
 }
 
 #[test]
