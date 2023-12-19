1569d869c726e4e84b8914b24c2ecb576785990d
Tim Balsfulland <tim@balsfulland.email>
2023-12-19 17:01:26 +0100

    simplify 2023 Day 19
    Not much prettier, but won't invest more time.

--- 2023/src/day19/mod.rs
+++ 2023/src/day19/mod.rs
@@ -1,11 +1,14 @@
 use std::cmp::{max, min};
 use std::collections::HashMap;
+use std::ops::Range;
 
-pub fn solve_first(input: &str) -> usize {
-    let (workflows, parts) = input.split_once("\r\n\r\n").unwrap();
-
+pub fn solve<'a>(
+    workflows: Vec<&'a str>,
+    mut stack: Vec<([Range<usize>; 4], &'a str)>,
+    combinations: bool,
+) -> usize {
     let workflows = workflows
-        .lines()
+        .into_iter()
         .map(|line| {
             let (name, rules) = line[..line.len() - 1].split_once('{').unwrap();
             let rules = rules
@@ -21,9 +24,9 @@ pub fn solve_first(input: &str) -> usize {
                         };
                         let comp = condition.chars().nth(1).unwrap();
                         let value = condition[2..].parse::<usize>().unwrap();
-                        (rating, comp, value, to)
+                        (Some(rating), comp, value, to)
                     } else {
-                        (4, '>', 0, rule)
+                        (None, ' ', 0, rule)
                     }
                 })
                 .collect::<Vec<_>>();
@@ -31,8 +34,61 @@ pub fn solve_first(input: &str) -> usize {
         })
         .collect::<HashMap<_, _>>();
 
-    parts
-        .lines()
+    let mut accepted = 0;
+
+    while let Some((mut ratings, current)) = stack.pop() {
+        if current == "A" {
+            if combinations {
+                // part 2
+                accepted += ratings.into_iter().map(|r| r.len()).product::<usize>();
+            } else {
+                // part 1
+                accepted += ratings.into_iter().map(|r| r.start).sum::<usize>();
+            }
+        } else if current != "R" {
+            for (rating, comp, value, to) in &workflows[current] {
+                if ratings.iter().any(|range| range.is_empty()) {
+                    continue;
+                }
+
+                if let Some(rating) = *rating {
+                    // split ranges at comparison point
+                    match comp {
+                        '<' if ratings[rating].start < *value => {
+                            let mut next = ratings.clone();
+                            next[rating].end = min(*value, next[rating].end);
+                            stack.push((next, *to));
+
+                            ratings[rating].start = *value;
+                        }
+                        '>' if ratings[rating].end >= *value => {
+                            let mut next = ratings.clone();
+                            next[rating].start = max(*value + 1, next[rating].start);
+                            stack.push((next, *to));
+
+                            ratings[rating].end = *value + 1;
+                        }
+                        _ => {}
+                    }
+                } else {
+                    // fall-through workflow
+                    stack.push((ratings.clone(), *to));
+                    break;
+                }
+            }
+        }
+    }
+
+    accepted
+}
+
+pub fn solve_first(input: &str) -> usize {
+    let mut lines = input.lines();
+    let workflows = lines.by_ref().take_while(|line| !line.is_empty()).collect();
+    let parts = lines;
+
+    // implement the first part as ranges with length 1
+    let parts = parts
         .map(|line| -> [_; 4] {
             line[1..line.len() - 1]
                 .split(',')
@@ -41,108 +97,20 @@ pub fn solve_first(input: &str) -> usize {
                 .try_into()
                 .unwrap()
         })
-        .filter(|ratings| {
-            let mut current = "in";
+        .map(|ratings| (ratings.map(|x| x..x + 1), "in"))
+        .collect();
 
-            while current != "A" && current != "R" {
-                for (rating, comp, value, to) in &workflows[current] {
-                    if *rating == 4 {
-                        current = *to;
-                        break;
-                    } else {
-                        match comp {
-                            '<' if ratings[*rating] < *value => {
-                                current = *to;
-                                break;
-                            }
-                            '>' if ratings[*rating] > *value => {
-                                current = *to;
-                                break;
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-            }
-
-            current == "A"
-        })
-        .map(|ratings| ratings.into_iter().sum::<usize>())
-        .sum()
+    solve(workflows, parts, false)
 }
 
 pub fn solve_second(input: &str) -> usize {
-    let (workflows, _) = input.split_once("\r\n\r\n").unwrap();
+    let workflows = input.lines().take_while(|line| !line.is_empty()).collect();
 
-    let workflows = workflows
-        .lines()
-        .map(|line| {
-            let (name, rules) = line[..line.len() - 1].split_once('{').unwrap();
-            let rules = rules
-                .split(',')
-                .map(|rule| {
-                    if let Some((condition, to)) = rule.split_once(':') {
-                        let rating = match condition.chars().next().unwrap() {
-                            'x' => 0_usize,
-                            'm' => 1,
-                            'a' => 2,
-                            's' => 3,
-                            _ => unreachable!(),
-                        };
-                        let comp = condition.chars().nth(1).unwrap();
-                        let value = condition[2..].parse::<usize>().unwrap();
-                        (rating, comp, value, to)
-                    } else {
-                        (4, '>', 0, rule)
-                    }
-                })
-                .collect::<Vec<_>>();
-            (name, rules)
-        })
-        .collect::<HashMap<_, _>>();
-
-    let mut stack = vec![([1_usize..4001, 1..4001, 1..4001, 1..4001], "in")];
-    let mut accepted = 0;
-
-    while let Some((mut ratings, current)) = stack.pop() {
-        if current == "A" {
-            accepted += ratings
-                .into_iter()
-                .map(|range| range.len())
-                .product::<usize>();
-        } else if current != "R" {
-            for (rating, comp, value, to) in &workflows[current] {
-                if ratings.iter().any(|range| range.is_empty()) {
-                    continue;
-                }
-
-                if *rating == 4 {
-                    stack.push((ratings.clone(), *to));
-                    break;
-                } else {
-                    match comp {
-                        '<' if ratings[*rating].start < *value => {
-                            let mut next = ratings.clone();
-                            next[*rating].end = min(*value, next[*rating].end);
-                            stack.push((next, *to));
-
-                            ratings[*rating].start = *value;
-                        }
-                        '>' if ratings[*rating].end >= *value => {
-                            let mut next = ratings.clone();
-                            next[*rating].start = max(*value + 1, next[*rating].start);
-                            stack.push((next, *to));
-
-                            ratings[*rating].end = *value + 1;
-                        }
-                        _ => {}
-                    }
-                }
-            }
-        }
-    }
-
-    accepted
+    solve(
+        workflows,
+        vec![([1_usize..4001, 1..4001, 1..4001, 1..4001], "in")],
+        true,
+    )
 }
 
 #[test]
