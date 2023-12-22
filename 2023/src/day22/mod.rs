5abf116fd7d68fd1a4ab27e9dbdc386dc1d634ed
Tim Balsfulland <tim@balsfulland.email>
2023-12-22 19:16:37 +0100

    simplify 2023 Day 22

--- 2023/src/day22/mod.rs
+++ 2023/src/day22/mod.rs
@@ -1,9 +1,9 @@
 #![allow(clippy::needless_range_loop)]
 
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-pub fn solve_first(input: &str) -> usize {
+fn create_grid(input: &str) -> (Vec<HashSet<usize>>, Vec<HashSet<usize>>) {
     let (mut max_x, mut max_y, mut max_z) = (0, 0, 0);
 
     let bricks = input
@@ -34,16 +34,14 @@ pub fn solve_first(input: &str) -> usize {
     let (width, depth, height) = (max_x + 1, max_y + 1, max_z + 1);
     let mut grid = vec![vec![vec![None::<usize>; height]; depth]; width];
 
-    // TODO is end_z >= start_z?
+    let mut supported_by = vec![HashSet::<usize>::new(); bricks.len()];
+    let mut supports = vec![HashSet::<usize>::new(); bricks.len()];
 
-    let mut supported_by = HashMap::<usize, HashSet<usize>>::new();
-    let mut supports = HashMap::<usize, HashSet<usize>>::new();
-
-    // fill grid with cubes
+    // fill grid with cubes in ascending z order
     for (i, (start, end)) in bricks
         .iter()
         .enumerate()
-        .sorted_unstable_by_key(|(_, ([_, _, start_z], [_, _, end_z]))| start_z.min(end_z))
+        .sorted_unstable_by_key(|(_, ([_, _, _], [_, _, end_z]))| end_z)
     {
         // search the height where the brick comes to rest
         let mut target_z = 1;
@@ -52,8 +50,8 @@ pub fn solve_first(input: &str) -> usize {
             for x in start[0]..=end[0] {
                 for y in start[1]..=end[1] {
                     if let Some(support) = grid[x][y][z] {
-                        supported_by.entry(i).or_default().insert(support);
-                        supports.entry(support).or_default().insert(i);
+                        supported_by[i].insert(support);
+                        supports[support].insert(i);
                         target_z = z + 1;
                         found = true;
                     }
@@ -75,91 +73,23 @@ pub fn solve_first(input: &str) -> usize {
         }
     }
 
-    (0..bricks.len())
-        .filter(|i| {
-            !supports.contains_key(i)
-                || supports[i]
-                    .iter()
-                    .all(|brick| supported_by[brick].len() > 1)
-        })
+    (supported_by, supports)
+}
+
+pub fn solve_first(input: &str) -> usize {
+    let (supported_by, supports) = create_grid(input);
+
+    supports
+        .iter()
+        .filter(|supports| supports.iter().all(|&brick| supported_by[brick].len() > 1))
         .count()
 }
 
 pub fn solve_second(input: &str) -> usize {
-    let (mut max_x, mut max_y, mut max_z) = (0, 0, 0);
+    let (supported_by, supports) = create_grid(input);
 
-    let bricks = input
-        .lines()
-        .map(|line| {
-            let (start, end) = line.split_once('~').unwrap();
-            let start: [_; 3] = start
-                .split(',')
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap();
-            let end: [_; 3] = end
-                .split(',')
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect::<Vec<_>>()
-                .try_into()
-                .unwrap();
-
-            max_x = max_x.max(start[0]).max(end[0]);
-            max_y = max_y.max(start[1]).max(end[1]);
-            max_z = max_z.max(start[2]).max(end[2]);
-
-            (start, end)
-        })
-        .collect::<Vec<_>>();
-
-    let (width, depth, height) = (max_x + 1, max_y + 1, max_z + 1);
-    let mut grid = vec![vec![vec![None::<usize>; height]; depth]; width];
-
-    // TODO is end_z >= start_z?
-
-    let mut supported_by = HashMap::<usize, HashSet<usize>>::new();
-    let mut supports = HashMap::<usize, HashSet<usize>>::new();
-
-    // fill grid with cubes
-    for (i, (start, end)) in bricks
-        .iter()
-        .enumerate()
-        .sorted_unstable_by_key(|(_, ([_, _, start_z], [_, _, end_z]))| start_z.min(end_z))
-    {
-        // search the height where the brick comes to rest
-        let mut target_z = 1;
-        for z in (1..start[2]).rev() {
-            let mut found = false;
-            for x in start[0]..=end[0] {
-                for y in start[1]..=end[1] {
-                    if let Some(support) = grid[x][y][z] {
-                        supported_by.entry(i).or_default().insert(support);
-                        supports.entry(support).or_default().insert(i);
-                        target_z = z + 1;
-                        found = true;
-                    }
-                }
-            }
-
-            if found {
-                break;
-            }
-        }
-
-        // place the brick at the height
-        for x in start[0]..=end[0] {
-            for y in start[1]..=end[1] {
-                for z in target_z..=(target_z + start[2].abs_diff(end[2])) {
-                    grid[x][y][z] = Some(i);
-                }
-            }
-        }
-    }
-
-    supports
-        .keys()
-        .map(|&initial| {
+    (0..supports.len())
+        .map(|initial| {
             let mut falls = HashSet::new();
 
             let mut new_falls = HashSet::new();
@@ -168,11 +98,12 @@ pub fn solve_second(input: &str) -> usize {
             while !new_falls.is_empty() {
                 falls.extend(new_falls.drain());
 
-                new_falls = (0..bricks.len())
-                    .filter(|brick| !falls.contains(brick))
-                    .filter(|brick| supported_by.contains_key(brick))
-                    .filter(|brick| supported_by[brick].difference(&falls).count() == 0)
-                    .collect();
+                new_falls.extend(
+                    (0..supported_by.len())
+                        .filter(|&brick| !supported_by[brick].is_empty())
+                        .filter(|brick| !falls.contains(brick))
+                        .filter(|&brick| supported_by[brick].difference(&falls).count() == 0),
+                );
             }
 
             falls.len() - 1
