3086bb142ede7cd9e0456f055b7ce9ffbdaedd7e
Tim Balsfulland <tim@balsfulland.email>
2023-12-25 16:00:42 +0100

    simplify 2023 Day 25
    mostly performance

--- 2023/src/day25/mod.rs
+++ 2023/src/day25/mod.rs
@@ -1,77 +1,87 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 
 pub fn solve(input: &str) -> usize {
-    let mut components = HashMap::<_, Vec<_>>::new();
+    let mut name_to_index = HashMap::new();
+    let mut components = Vec::new();
+    let insert_index = |components: &mut Vec<Vec<usize>>| {
+        let index = components.len();
+        components.push(Vec::new());
+        index
+    };
     for line in input.lines() {
         let (from, to) = line.split_once(": ").unwrap();
+        let from = *name_to_index
+            .entry(from)
+            .or_insert_with(|| insert_index(&mut components));
+
         for to in to.split_whitespace() {
-            components.entry(from).or_default().push(to);
-            components.entry(to).or_default().push(from);
+            let to = *name_to_index
+                .entry(to)
+                .or_insert_with(|| insert_index(&mut components));
+
+            components[from].push(to);
+            components[to].push(from);
         }
     }
-    let mut name_to_index = HashMap::new();
-    let components = components
-        .into_iter()
-        .enumerate()
-        .map(|(i, (name, to))| {
-            name_to_index.insert(name, i);
-            to
-        })
-        .collect::<Vec<_>>();
-    let components = components
-        .into_iter()
-        .map(|to| {
-            to.into_iter()
-                .map(|to| name_to_index[to])
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
 
     // https://en.wikipedia.org/wiki/Edmonds%E2%80%93Karp_algorithm
     // There is probably a faster way
 
     let start = 0;
     let mut count = 1;
+
+    // `queue` and `removed_edges` get reset inside the loop to save allocations
+    let mut queue = VecDeque::new();
+    let mut removed_edges = vec![false; components.len() * components.len()];
+
     'outer: for end in 1..components.len() {
-        // try to find 4 unique paths from `start` to `end`
-        let mut removed_edges = HashSet::<(usize, usize)>::new();
+        // try to find 4 paths with unique edges from `start` to `end`
+
+        // reset `removed_edges` -> all edges are active again
+        for removed in &mut removed_edges {
+            *removed = false;
+        }
 
         for _ in 0..4 {
-            let mut queue = VecDeque::new();
-            let mut pred = vec![None; components.len()];
+            let mut predecessor = vec![None; components.len()];
+
+            queue.clear();
             queue.push_back(start);
+
             while let Some(current) = queue.pop_front() {
                 if current == end {
                     break;
                 }
 
                 for &neighbor in &components[current] {
-                    if pred[neighbor].is_none()
-                        && !removed_edges.contains(&(current, neighbor))
-                        && !removed_edges.contains(&(neighbor, current))
+                    if predecessor[neighbor].is_none()
+                        && !removed_edges[current + neighbor * components.len()]
                     {
-                        pred[neighbor] = Some(current);
+                        predecessor[neighbor] = Some(current);
                         queue.push_back(neighbor);
                     }
                 }
             }
 
-            if pred[end].is_none() {
-                // No 4 paths from `start` to `end`
+            if predecessor[end].is_none() {
+                // no 4 paths from `start` to `end`
                 continue 'outer;
             }
 
-            // "remove" edges by walking the path from the end
+            // "remove" edges walking the path from the end
             let mut current = end;
             while current != start {
-                let pred = pred[current].unwrap();
+                let pred = predecessor[current].unwrap();
 
-                removed_edges.insert((pred, current));
+                removed_edges[pred + current * components.len()] = true;
+                removed_edges[current + pred * components.len()] = true;
 
                 current = pred;
             }
         }
 
+        // successfully found 4 paths with unique edges from `start` to `end`
+        // -> they are part of the same component in the solution
         count += 1;
     }
 
