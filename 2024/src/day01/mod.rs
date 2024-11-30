f998c0fcf0c0d870c935225d301937cf0d2df020
Tim Balsfulland <tim@balsfulland.email>
2024-11-30 01:02:17 +0100

    2024 preparation
    - init 2024 crate
    - remove IDE files

---
+++ 2024/src/day01/mod.rs
@@ -0,0 +1,20 @@
+pub fn solve_first(input: &str) -> u32 {
+    todo!()
+}
+
+pub fn solve_second(input: &str) -> u32 {
+    todo!()
+}
+
+#[test]
+pub fn sample() {
+    assert_eq!(0, solve_first(include_str!("sample.txt")));
+    assert_eq!(0, solve_second(include_str!("sample.txt")));
+}
+
+#[test]
+pub fn input() {
+    let input = include_str!("input.txt");
+    assert_eq!(0, solve_first(input));
+    assert_eq!(0, solve_second(input));
+}
