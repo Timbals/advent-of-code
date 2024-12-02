07d34cfad8f30cdd91678390ef1bb196557f6db5
Tim Balsfulland <tim@balsfulland.email>
2024-12-02 14:08:28 +0100

    simplify 2024 Day 2

--- 2024/src/day02/mod.rs
+++ 2024/src/day02/mod.rs
@@ -1,50 +1,40 @@
 use itertools::Itertools;
 
-pub fn solve_first(input: &str) -> u32 {
-    input
-        .lines()
-        .filter(|report| {
-            let mut levels =
-                report.split_whitespace().map(|x| x.parse::<u32>().unwrap()).tuple_windows();
-
-            (levels.clone().all(|(a, b)| a < b) || levels.clone().all(|(a, b)| a > b))
-                && levels.all(|(a, b)| (1..=3).contains(&a.abs_diff(b)))
-        })
-        .count() as u32
+fn safe(report: impl IntoIterator<Item = usize>) -> bool {
+    let mut increasing = true;
+    let mut decreasing = true;
+    for (a, b) in report.into_iter().tuple_windows() {
+        increasing &= a < b && (1..=3).contains(&(b - a));
+        decreasing &= a > b && (1..=3).contains(&(a - b));
+    }
+    increasing || decreasing
 }
 
-pub fn solve_second(input: &str) -> u32 {
+pub fn solve_first(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|report| safe(report.split_whitespace().map(|x| x.parse::<usize>().unwrap())))
+        .count()
+}
+
+pub fn solve_second(input: &str) -> usize {
     input
         .lines()
         .filter(|report| {
             let levels =
-                report.split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect::<Vec<_>>();
-
-            let mut safe = false;
-            for i in 0..levels.len() {
-                let mut levels = levels.clone();
-                levels.remove(i);
-                safe |= (levels.iter().tuple_windows().all(|(a, b)| a < b)
-                    || levels.iter().tuple_windows().all(|(a, b)| a > b))
-                    && levels
-                        .iter()
-                        .tuple_windows()
-                        .all(|(a, b)| (1..=3).contains(&a.abs_diff(*b)));
-            }
-
-            safe |= (levels.iter().tuple_windows().all(|(a, b)| a < b)
-                || levels.iter().tuple_windows().all(|(a, b)| a > b))
-                && levels.iter().tuple_windows().all(|(a, b)| (1..=3).contains(&a.abs_diff(*b)));
-
-            safe
+                report.split_whitespace().map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+            (0..=levels.len()).any(|skip| {
+                safe(levels.iter().enumerate().filter(|&(i, _)| skip != i).map(|(_, x)| *x))
+            })
         })
-        .count() as u32
+        .count()
 }
 
 #[test]
 pub fn sample() {
-    assert_eq!(2, solve_first(include_str!("sample.txt")));
-    assert_eq!(4, solve_second(include_str!("sample.txt")));
+    let sample = include_str!("sample.txt");
+    assert_eq!(2, solve_first(sample));
+    assert_eq!(4, solve_second(sample));
 }
 
 #[test]
