13bdbdbdef09c7fe94867373f6750d5b691b813f
Tim Balsfulland <tim@balsfulland.email>
2024-12-02 14:08:51 +0100

    use `usize` instead of `u32`

--- 2024/src/day01/mod.rs
+++ 2024/src/day01/mod.rs
@@ -1,28 +1,28 @@
 use std::collections::HashMap;
 use std::iter::zip;
 
-pub fn solve_first(input: &str) -> u32 {
+pub fn solve_first(input: &str) -> usize {
     let mut left = Vec::new();
     let mut right = Vec::new();
     for line in input.lines() {
         let (x, y) = line.split_once("   ").unwrap();
-        left.push(x.parse::<u32>().unwrap());
-        right.push(y.parse::<u32>().unwrap());
+        left.push(x.parse::<usize>().unwrap());
+        right.push(y.parse::<usize>().unwrap());
     }
 
     left.sort_unstable();
     right.sort_unstable();
 
-    zip(left, right).map(|(x, y)| u32::abs_diff(x, y)).sum()
+    zip(left, right).map(|(x, y)| usize::abs_diff(x, y)).sum()
 }
 
-pub fn solve_second(input: &str) -> u32 {
+pub fn solve_second(input: &str) -> usize {
     let mut left = Vec::new();
     let mut right = HashMap::new();
     for line in input.lines() {
         let (x, y) = line.split_once("   ").unwrap();
-        left.push(x.parse::<u32>().unwrap());
-        *right.entry(y.parse::<u32>().unwrap()).or_default() += 1;
+        left.push(x.parse::<usize>().unwrap());
+        *right.entry(y.parse::<usize>().unwrap()).or_default() += 1;
     }
 
     left.into_iter().map(|x| x * *right.get(&x).unwrap_or(&0)).sum()
@@ -30,8 +30,9 @@ pub fn solve_second(input: &str) -> u32 {
 
 #[test]
 pub fn sample() {
-    assert_eq!(11, solve_first(include_str!("sample.txt")));
-    assert_eq!(31, solve_second(include_str!("sample.txt")));
+    let sample = include_str!("sample.txt");
+    assert_eq!(11, solve_first(sample));
+    assert_eq!(31, solve_second(sample));
 }
 
 #[test]
