e0cb9186638040c7527facb370bb05fee4da1d8f
Tim Balsfulland <tim@balsfulland.email>
2024-12-03 14:47:09 +0100

    simplify 2024 Day 3
    This one was made for regex :)

--- 2024/src/day03/mod.rs
+++ 2024/src/day03/mod.rs
@@ -1,129 +1,33 @@
-use itertools::Itertools;
+use regex::Regex;
 
-pub fn solve_first(input: &str) -> u32 {
+pub fn solve(input: &str, enabler: bool) -> usize {
     let mut result = 0;
-    for (i, _) in input.match_indices("mul(") {
-        let input = &input[(i + 4)..];
-        let mut input = input.chars().peekable();
-        if let Some(x) = input.next().and_then(|x| x.to_digit(10)) {
-            let mut first = x;
-            if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                input.next();
-                first *= 10;
-                first += x;
-
-                if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                    input.next();
-                    first *= 10;
-                    first += x;
-                }
-            }
-
-            if input.next() != Some(',') {
-                continue;
-            }
-
-            if let Some(x) = input.next().and_then(|x| x.to_digit(10)) {
-                let mut second = x;
-                if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                    input.next();
-                    second *= 10;
-                    second += x;
-
-                    if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                        input.next();
-                        second *= 10;
-                        second += x;
-                    }
-                }
-
-                if input.next() != Some(')') {
-                    continue;
-                }
-
-                result += first * second;
-            }
-        }
-    }
-    result
-}
-
-pub fn solve_second(input: &str) -> u32 {
     let mut enabled = true;
-    let enables = input.match_indices("do()");
-    let disables = input.match_indices("don't()");
-    let mut enables_disables = enables.merge_by(disables, |(a, _), (b, _)| a < b).peekable();
 
-    let mut result = 0;
-    for (i, _) in input.match_indices("mul(") {
-        while enables_disables.peek().map(|(j, _)| *j).unwrap_or(usize::MAX) < i {
-            let (_, action) = enables_disables.next().unwrap();
-            print!("{action}");
-            match action {
-                "do()" => enabled = true,
-                "don't()" => enabled = false,
-                _ => unreachable!(),
-            }
-        }
-
-        if !enabled {
-            continue;
-        }
-
-        let input = &input[(i + 4)..];
-        let mut input = input.chars().peekable();
-        if let Some(x) = input.next().and_then(|x| x.to_digit(10)) {
-            let mut first = x;
-            if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                input.next();
-                first *= 10;
-                first += x;
-
-                if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                    input.next();
-                    first *= 10;
-                    first += x;
-                }
-            }
-
-            if input.next() != Some(',') {
-                continue;
-            }
-
-            if let Some(x) = input.next().and_then(|x| x.to_digit(10)) {
-                let mut second = x;
-                if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                    input.next();
-                    second *= 10;
-                    second += x;
-
-                    if let Some(x) = input.peek().and_then(|x| x.to_digit(10)) {
-                        input.next();
-                        second *= 10;
-                        second += x;
-                    }
-                }
-
-                if input.next() != Some(')') {
-                    continue;
-                }
-
-                result += first * second;
-            }
+    let re = Regex::new("mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)").unwrap();
+    for captures in re.captures_iter(input) {
+        if enabled && captures.get(1).is_some() {
+            result += captures.get(1).unwrap().as_str().parse::<usize>().unwrap()
+                * captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        } else if enabler && captures.get(0).unwrap().as_str() == "do()" {
+            enabled = true;
+        } else if enabler && captures.get(0).unwrap().as_str() == "don't()" {
+            enabled = false;
         }
     }
+
     result
 }
 
 #[test]
 pub fn sample() {
-    assert_eq!(161, solve_first(include_str!("sample1.txt")));
-    assert_eq!(48, solve_second(include_str!("sample2.txt")));
+    assert_eq!(161, solve(include_str!("sample1.txt"), false));
+    assert_eq!(48, solve(include_str!("sample2.txt"), true));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(160672468, solve_first(input));
-    assert_eq!(84893551, solve_second(input));
+    assert_eq!(160672468, solve(input, false));
+    assert_eq!(84893551, solve(input, true));
 }
