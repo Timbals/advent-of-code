936b238cd8b579fa9b3bd1dedb087aa76fece2a5
Tim Balsfulland <tim@balsfulland.email>
2024-12-04 15:56:38 +0100

    simplify 2024 Day 4

--- 2024/src/day04/mod.rs
+++ 2024/src/day04/mod.rs
@@ -1,107 +1,44 @@
-use itertools::chain;
-use std::iter::once;
-
 pub fn solve_first(input: &str) -> usize {
-    let mut result = 0;
-
-    // horizontal
-    result += input.matches("XMAS").count();
-    // horizontal backwards
-    result += input.matches("SAMX").count();
-
-    let lines = input.lines().collect::<Vec<_>>();
+    let lines = input.lines().map(|x| x.as_bytes()).collect::<Vec<_>>();
     let width = lines[0].len();
     let height = lines.len();
 
-    let lines_ref = &lines;
-    let columns = (0..width).flat_map(|x| {
-        (0..height).map(move |y| lines_ref[y].as_bytes()[x] as char).chain(once(' '))
-    });
-    let columns = columns.collect::<String>();
+    let mut result = 0;
 
-    // vertical
-    result += columns.matches("XMAS").count();
-    // vertical backwards
-    result += columns.matches("SAMX").count();
-
-    let diagonals_x_positive = (0..width).flat_map(|x| {
-        let mut i = 0;
-        std::iter::from_fn(move || {
-            let y = i;
-            let x = x + i;
-            if x < width && y < height {
-                i += 1;
-                Some(lines_ref[y].as_bytes()[x] as char)
-            } else {
-                None
+    let steps = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)];
+    for y in 0..height {
+        for x in 0..width {
+            if lines.get(y).and_then(|row| row.get(x)) == Some(&b'X') {
+                for (dx, dy) in steps {
+                    if lines
+                        .get(y.wrapping_add_signed(dy))
+                        .and_then(|row| row.get(x.wrapping_add_signed(dx)))
+                        == Some(&b'M')
+                        && lines
+                            .get(y.wrapping_add_signed(dy * 2))
+                            .and_then(|row| row.get(x.wrapping_add_signed(dx * 2)))
+                            == Some(&b'A')
+                        && lines
+                            .get(y.wrapping_add_signed(dy * 3))
+                            .and_then(|row| row.get(x.wrapping_add_signed(dx * 3)))
+                            == Some(&b'S')
+                    {
+                        result += 1;
+                    }
+                }
             }
-        })
-        .chain(once(' '))
-    });
-    let diagonals_y_positive = (1..height).flat_map(|y| {
-        let mut i = 0;
-        std::iter::from_fn(move || {
-            let x = i;
-            let y = y + i;
-            if x < width && y < height {
-                i += 1;
-                Some(lines_ref[y].as_bytes()[x] as char)
-            } else {
-                None
-            }
-        })
-        .chain(once(' '))
-    });
-    let diagonals_x_negative = (0..width).flat_map(|x| {
-        let mut i = 0;
-        std::iter::from_fn(move || {
-            let y = i;
-            let x = x as isize - i;
-            if x >= 0 && y < height as isize {
-                i += 1;
-                Some(lines_ref[y as usize].as_bytes()[x as usize] as char)
-            } else {
-                None
-            }
-        })
-        .chain(once(' '))
-    });
-    let diagonals_y_negative = (1..height).flat_map(|y| {
-        let mut i = 0;
-        std::iter::from_fn(move || {
-            let x = width as isize - i - 1;
-            let y = y as isize + i;
-            if x >= 0 && y < height as isize {
-                i += 1;
-                Some(lines_ref[y as usize].as_bytes()[x as usize] as char)
-            } else {
-                None
-            }
-        })
-        .chain(once(' '))
-    });
-
-    let diagonals = chain!(
-        diagonals_x_positive,
-        diagonals_y_positive,
-        diagonals_x_negative,
-        diagonals_y_negative
-    )
-    .collect::<String>();
-    result += diagonals.matches("XMAS").count();
-    result += diagonals.matches("SAMX").count();
+        }
+    }
 
     result
 }
 
 pub fn solve_second(input: &str) -> usize {
-    let mut result = 0;
-
     let lines = input.lines().map(|line| line.as_bytes()).collect::<Vec<_>>();
     let width = lines[0].len();
     let height = lines.len();
 
-    let diff = b'M'.abs_diff(b'S');
+    let mut result = 0;
 
     for y in 0..(height - 2) {
         for x in 0..(width - 2) {
@@ -110,15 +47,12 @@ pub fn solve_second(input: &str) -> usize {
             let bottom_left = lines[y + 2][x];
             let bottom_right = lines[y + 2][x + 2];
             let middle = lines[y + 1][x + 1];
-            if (top_left == b'M' || top_left == b'S')
-                && (top_right == b'M' || top_right == b'S')
-                && (bottom_left == b'M' || bottom_left == b'S')
-                && (bottom_right == b'M' || bottom_right == b'S')
-                && middle == b'A'
-                && top_left.abs_diff(bottom_right) == diff
-                && top_right.abs_diff(bottom_left) == diff
+            if let (b'M' | b'S', b'M' | b'S', b'M' | b'S', b'M' | b'S', b'A') =
+                (top_left, top_right, bottom_left, bottom_right, middle)
             {
-                result += 1;
+                if top_left != bottom_right && top_right != bottom_left {
+                    result += 1;
+                }
             }
         }
     }
