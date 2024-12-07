6a638f298726037f5f38e97983f7f826d32712dd
Tim Balsfulland <tim@balsfulland.email>
2024-12-07 18:05:18 +0100

    simplify 2024 Day 7

--- 2024/src/day07/mod.rs
+++ 2024/src/day07/mod.rs
@@ -2,53 +2,29 @@ use itertools::Itertools;
 use std::iter::{repeat_n, zip};
 use std::ops::{Add, Mul};
 
-pub fn solve_first(input: &str) -> usize {
+pub fn solve(input: &str, concatenation: bool) -> u64 {
+    let operators = if concatenation {
+        vec![u64::add, u64::mul, |a: u64, b: u64| a * 10_u64.pow(b.ilog10() + 1) + b]
+    } else {
+        vec![u64::add, u64::mul]
+    };
+
     input
         .lines()
         .filter_map(|line| {
             let (value, equation) = line.split_once(": ").unwrap();
-            let value = value.parse::<usize>().unwrap();
-            let equation = equation
-                .split_whitespace()
-                .map(|x| x.parse::<usize>().unwrap())
-                .collect::<Vec<_>>();
-            repeat_n([usize::add, usize::mul], equation.len() - 1)
+            let value = value.parse::<u64>().unwrap();
+            let equation =
+                equation.split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<_>>();
+            repeat_n(&operators, equation.len() - 1)
                 .multi_cartesian_product()
                 .any(|operators| {
                     let mut equation = equation.iter();
-                    let mut result = *equation.next().unwrap();
-                    for (operand, operator) in zip(equation, operators) {
-                        result = operator(result, *operand);
-                    }
-                    result == value
+                    let first = *equation.next().unwrap();
+                    zip(equation, operators).fold(first, |a, (b, operator)| operator(a, *b))
+                        == value
                 })
                 .then_some(value)
-        })
-        .sum()
-}
-
-pub fn solve_second(input: &str) -> u128 {
-    input
-        .lines()
-        .filter_map(|line| {
-            let (value, equation) = line.split_once(": ").unwrap();
-            let value = value.parse::<u128>().unwrap();
-            let equation =
-                equation.split_whitespace().map(|x| x.parse::<u128>().unwrap()).collect::<Vec<_>>();
-            repeat_n(
-                [u128::add, u128::mul, |a: u128, b: u128| a * 10_u128.pow(b.ilog10() + 1) + b],
-                equation.len() - 1,
-            )
-            .multi_cartesian_product()
-            .any(|operators| {
-                let mut equation = equation.iter();
-                let mut result = *equation.next().unwrap();
-                for (operand, operator) in zip(equation, operators) {
-                    result = operator(result, *operand);
-                }
-                result == value
-            })
-            .then_some(value)
         })
         .sum()
 }
@@ -56,13 +32,13 @@ pub fn solve_second(input: &str) -> u128 {
 #[test]
 pub fn sample() {
     let sample = include_str!("sample.txt");
-    assert_eq!(3749, solve_first(sample));
-    assert_eq!(11387, solve_second(sample));
+    assert_eq!(3749, solve(sample, false));
+    assert_eq!(11387, solve(sample, true));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(2314935962622, solve_first(input));
-    assert_eq!(401477450831495, solve_second(input));
+    assert_eq!(2314935962622, solve(input, false));
+    assert_eq!(401477450831495, solve(input, true));
 }
