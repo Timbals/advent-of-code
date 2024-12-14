a430e4df7084abc7b6d26286580b63ab7d20311a
Tim Balsfulland <tim@balsfulland.email>
2024-12-14 15:37:33 +0100

    simplify 2024 Day 14

--- 2024/src/day14/mod.rs
+++ 2024/src/day14/mod.rs
@@ -1,19 +1,22 @@
 use itertools::Itertools;
 use std::cmp::Ordering;
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeSet;
 
-pub fn solve_first(input: &str, width: isize, height: isize) -> usize {
-    let robots = input.lines().map(|line| {
+fn parse(input: &str) -> impl Iterator<Item = (isize, isize, isize, isize)> + use<'_> {
+    input.lines().map(|line| {
         let (p, v) = line.strip_prefix("p=").unwrap().split_once(" v=").unwrap();
         let (px, py) = p.split_once(',').unwrap();
         let (px, py) = (px.parse::<isize>().unwrap(), py.parse::<isize>().unwrap());
         let (vx, vy) = v.split_once(',').unwrap();
         let (vx, vy) = (vx.parse::<isize>().unwrap(), vy.parse::<isize>().unwrap());
         (px, py, vx, vy)
-    });
+    })
+}
+
+pub fn solve_first(input: &str, width: isize, height: isize) -> usize {
+    let robots = parse(input);
 
     let (mut q1, mut q2, mut q3, mut q4) = (0, 0, 0, 0);
-
     for (px, py, vx, vy) in robots {
         let px = (px + 100 * vx).rem_euclid(width);
         let py = (py + 100 * vy).rem_euclid(height);
@@ -30,55 +33,33 @@ pub fn solve_first(input: &str, width: isize, height: isize) -> usize {
 }
 
 pub fn solve_second(input: &str, width: isize, height: isize) -> isize {
-    let robots = input.lines().map(|line| {
-        let (p, v) = line.strip_prefix("p=").unwrap().split_once(" v=").unwrap();
-        let (px, py) = p.split_once(',').unwrap();
-        let (px, py) = (px.parse::<isize>().unwrap(), py.parse::<isize>().unwrap());
-        let (vx, vy) = v.split_once(',').unwrap();
-        let (vx, vy) = (vx.parse::<isize>().unwrap(), vy.parse::<isize>().unwrap());
-        (px, py, vx, vy)
-    });
+    let robots = parse(input).collect::<Vec<_>>();
 
-    let mut map = BTreeMap::new();
+    (0..10000)
+        .max_by_key(|i| {
+            let mut positions = BTreeSet::new();
+            for (px, py, vx, vy) in &robots {
+                let px = (px + i * vx).rem_euclid(width);
+                let py = (py + i * vy).rem_euclid(height);
+                positions.insert((px, py));
+            }
 
-    for i in 0..10000 {
-        let mut positions = BTreeSet::new();
-        for (px, py, vx, vy) in robots.clone() {
-            let px = (px + i * vx).rem_euclid(width);
-            let py = (py + i * vy).rem_euclid(height);
-            positions.insert((px, py));
-        }
-
-        // detect very long run of pixels
-        let (_, _, run_length) = positions
-            .iter()
-            .map(|(x, y)| (x, y, 1))
-            .coalesce(|prev, curr| {
-                if prev.0 == curr.0 && prev.1.abs_diff(*curr.1) == 1 {
-                    Ok((curr.0, curr.1, prev.2 + 1))
-                } else {
-                    Err((prev, curr))
-                }
-            })
-            .max_by_key(|(_, _, run_length)| *run_length)
-            .unwrap();
-        map.insert(run_length, i);
-
-        // println!("{i}:");
-        // for y in 0..height {
-        //     for x in 0..width {
-        //         if positions.contains(&(x, y)) {
-        //             print!("█");
-        //         } else {
-        //             print!(" ");
-        //         }
-        //     }
-        //     println!();
-        // }
-        // std::thread::sleep_ms(1);
-    }
-
-    map.pop_last().unwrap().1
+            // detect very long run of pixels
+            positions
+                .into_iter()
+                .map(|(x, y)| (x, y, 1))
+                .coalesce(|prev, curr| {
+                    if prev.0 == curr.0 && prev.1.abs_diff(curr.1) == 1 {
+                        Ok((curr.0, curr.1, prev.2 + 1))
+                    } else {
+                        Err((prev, curr))
+                    }
+                })
+                .max_by_key(|(_, _, run_length)| *run_length)
+                .unwrap()
+                .2
+        })
+        .unwrap()
 }
 
 #[test]
