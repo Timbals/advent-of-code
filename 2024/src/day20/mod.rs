19aed6d4f084f3490dbe0337b3c2d6c507eb3702
Tim Balsfulland <tim@balsfulland.email>
2024-12-20 18:06:39 +0100

    simplify 2024 Day 20

--- 2024/src/day20/mod.rs
+++ 2024/src/day20/mod.rs
@@ -1,53 +1,6 @@
 use itertools::Itertools;
-use std::collections::{BTreeMap, HashSet, VecDeque};
 
-pub fn solve_first(input: &str, save: usize) -> usize {
-    let grid = input.lines().map(|line| line.as_bytes()).collect::<Vec<_>>();
-    let width = grid[0].len();
-    let height = grid.len();
-
-    let (sy, sx) =
-        (0..height).cartesian_product(0..width).find(|&(y, x)| grid[y][x] == b'S').unwrap();
-
-    let mut stack = VecDeque::new();
-    stack.push_back((0, sx, sy, None, None));
-    let mut visited = HashSet::new();
-    visited.insert((sx, sy, None, None));
-    let mut times = Vec::new();
-    let mut normal_time = usize::MAX;
-    while let Some((steps, x, y, cheat_start, cheat_end)) = stack.pop_front() {
-        if grid[y][x] == b'E' {
-            if cheat_start.is_none() {
-                normal_time = steps;
-                break;
-            }
-
-            times.push(steps);
-            continue;
-        }
-
-        for (dx, dy) in [(1, 0), (0, 1), (-1, 0), (0, -1)] {
-            let (nx, ny) = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
-            if let Some(&b) = grid.get(ny).and_then(|row| row.get(nx)) {
-                if b == b'#' {
-                    if (cheat_start.is_none()) && visited.insert((nx, ny, Some(steps), cheat_end)) {
-                        stack.push_back((steps + 1, nx, ny, Some(steps), None));
-                    }
-                } else if visited.insert((nx, ny, cheat_start, cheat_end)) {
-                    if cheat_start.is_some() && cheat_end.is_none() {
-                        stack.push_back((steps + 1, nx, ny, cheat_start, Some((nx, ny))));
-                    } else {
-                        stack.push_back((steps + 1, nx, ny, cheat_start, cheat_end));
-                    }
-                }
-            }
-        }
-    }
-
-    times.into_iter().filter(|&time| time <= normal_time - save).count()
-}
-
-pub fn solve_second(input: &str, save: usize, cheat: isize) -> usize {
+pub fn solve(input: &str, save: usize, cheat: isize) -> usize {
     let grid = input.lines().map(|line| line.as_bytes()).collect::<Vec<_>>();
     let width = grid[0].len();
     let height = grid.len();
@@ -65,31 +18,23 @@ pub fn solve_second(input: &str, save: usize, cheat: isize) -> usize {
             }
         }
     }
-    let shortcut =
-        path.iter().rev().enumerate().map(|(i, &coord)| (coord, i)).collect::<BTreeMap<_, _>>();
-    // println!("{path:?} {}", path.len());
-    // println!("{shortcut:?} {}", path.len());
+    let mut shortcut = vec![vec![usize::MAX; width]; height];
+    for (remaining_steps, &(x, y)) in path.iter().rev().enumerate() {
+        shortcut[y][x] = remaining_steps;
+    }
 
     let mut result = 0;
     for (start_steps, &(x, y)) in path.iter().enumerate() {
-        let mut end = Vec::new();
         for dy in (-cheat)..=cheat {
             let remaining = cheat - dy.abs();
             for dx in -remaining..=remaining {
                 let (nx, ny) = (x.wrapping_add_signed(dx), y.wrapping_add_signed(dy));
-                if let Some(&b) = grid.get(ny).and_then(|row| row.get(nx)) {
-                    if b != b'#' {
-                        end.push((nx, ny));
+                if nx < width && ny < height && grid[ny][nx] != b'#' {
+                    let steps = start_steps + x.abs_diff(nx) + y.abs_diff(ny) + shortcut[ny][nx];
+                    if steps + save <= path.len() - 1 {
+                        result += 1;
                     }
                 }
-            }
-        }
-
-        for (nx, ny) in end {
-            let steps = start_steps + x.abs_diff(nx) + y.abs_diff(ny) + shortcut[&(nx, ny)];
-            if steps + save <= path.len() - 1 {
-                // println!("({x} {y}) -> ({nx} {ny}) {}", path.len() - 1 - steps);
-                result += 1;
             }
         }
     }
@@ -100,16 +45,13 @@ pub fn solve_second(input: &str, save: usize, cheat: isize) -> usize {
 #[test]
 pub fn sample() {
     let sample = include_str!("sample.txt");
-    assert_eq!(44, solve_second(sample, 2, 2));
-    assert_eq!(
-        32 + 31 + 29 + 39 + 25 + 23 + 20 + 19 + 12 + 14 + 12 + 22 + 4 + 3,
-        solve_second(sample, 50, 20)
-    );
+    assert_eq!(44, solve(sample, 2, 2));
+    assert_eq!(285, solve(sample, 50, 20));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(1375, solve_second(input, 100, 2));
-    assert_eq!(983054, solve_second(input, 100, 20));
+    assert_eq!(1375, solve(input, 100, 2));
+    assert_eq!(983054, solve(input, 100, 20));
 }
