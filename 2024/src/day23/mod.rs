620167cf4fea7b91e1008abbbfac6a6e04c00f9d
Tim Balsfulland <tim@balsfulland.email>
2024-12-23 13:19:14 +0100

    2024 Day 23

---
+++ 2024/src/day23/mod.rs
@@ -0,0 +1,63 @@
+use itertools::Itertools;
+use std::collections::{BTreeSet, HashMap, HashSet};
+
+pub fn solve_first(input: &str) -> usize {
+    let mut neighbors = HashMap::<&str, HashSet<&str>>::new();
+    for (from, to) in input.lines().map(|line| line.split_once('-').unwrap()) {
+        neighbors.entry(from).or_default().insert(to);
+        neighbors.entry(to).or_default().insert(from);
+    }
+
+    let mut three_components = HashSet::new();
+    for &node in neighbors.keys().filter(|node| node.starts_with('t')) {
+        for &neighbor1 in &neighbors[node] {
+            for &neighbor2 in &neighbors[neighbor1] {
+                for &neighbor3 in &neighbors[neighbor2] {
+                    if neighbor3 == node {
+                        three_components.insert(BTreeSet::from([node, neighbor1, neighbor2]));
+                    }
+                }
+            }
+        }
+    }
+
+    three_components.len()
+}
+
+pub fn solve_second(input: &str) -> String {
+    let mut neighbors = HashMap::<&str, BTreeSet<&str>>::new();
+    for (from, to) in input.lines().map(|line| line.split_once('-').unwrap()) {
+        neighbors.entry(from).or_default().insert(to);
+        neighbors.entry(to).or_default().insert(from);
+    }
+
+    let mut components =
+        neighbors.keys().map(|&node| BTreeSet::from([node])).collect::<HashSet<_>>();
+    while components.len() > 1 {
+        for component in std::mem::take(&mut components) {
+            for (&node, neighbors) in &neighbors {
+                if component.is_subset(neighbors) {
+                    let mut component = component.clone();
+                    component.insert(node);
+                    components.insert(component);
+                }
+            }
+        }
+    }
+
+    components.into_iter().exactly_one().unwrap().into_iter().sorted_unstable().join(",")
+}
+
+#[test]
+pub fn sample() {
+    let sample = include_str!("sample.txt");
+    assert_eq!(7, solve_first(sample));
+    assert_eq!("co,de,ka,ta", solve_second(sample));
+}
+
+#[test]
+pub fn input() {
+    let input = include_str!("input.txt");
+    assert_eq!(1046, solve_first(input));
+    assert_eq!("de,id,ke,ls,po,sn,tf,tl,tm,uj,un,xw,yz", solve_second(input));
+}
