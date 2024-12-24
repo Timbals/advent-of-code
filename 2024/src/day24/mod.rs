070d52c17ef6fa8c1d3ad36e4f6f7845e00ccb8f
Tim Balsfulland <tim@balsfulland.email>
2024-12-24 15:56:48 +0100

    simplify 2024 Day 24

--- 2024/src/day24/mod.rs
+++ 2024/src/day24/mod.rs
@@ -1,5 +1,5 @@
 use itertools::Itertools;
-use std::collections::{BTreeSet, HashMap, HashSet};
+use std::collections::{BTreeSet, HashMap};
 
 pub fn solve_first(input: &str) -> u64 {
     let mut lines = input.lines();
@@ -50,96 +50,72 @@ pub fn solve_first(input: &str) -> u64 {
 }
 
 pub fn solve_second(input: &str) -> String {
-    let mut lines = input.lines();
-    let mut _states = lines
-        .by_ref()
-        .take_while(|line| !line.is_empty())
-        .map(|line| {
-            let (wire, state) = line.split_once(": ").unwrap();
-            let state = state.parse::<u8>().unwrap() == 1;
-            (wire, state)
-        })
-        .collect::<HashMap<&str, bool>>();
-
-    let mut gates = lines
+    let mut gates = input
+        .lines()
+        .skip_while(|line| !line.is_empty())
+        .skip(1)
         .map(|line| {
             let (input, out) = line.split_once(" -> ").unwrap();
             let mut input = input.split_whitespace();
             let a = input.next().unwrap();
             let op = input.next().unwrap();
             let b = input.next().unwrap();
-            (out, (op, BTreeSet::from([a, b])))
+            ((op, BTreeSet::from([a, b])), out)
         })
         .collect::<HashMap<_, _>>();
-    let mut gates_reverse = gates.iter().map(|(&k, v)| (v.clone(), k)).collect::<HashMap<_, _>>();
+
+    let op_map = gates
+        .iter()
+        .flat_map(|((op, ops), out)| {
+            let ops = ops.iter().copied().collect::<Vec<_>>();
+            [
+                (BTreeSet::from([out, ops[1], op]), ops[0]),
+                (BTreeSet::from([out, ops[0], op]), ops[1]),
+            ]
+        })
+        .collect::<HashMap<_, _>>();
 
     let mut result = Vec::<&str>::new();
-    let mut carry = gates_reverse[&("AND", BTreeSet::from(["x00", "y00"]))];
+    let mut carry = gates[&("AND", BTreeSet::from(["x00", "y00"]))];
+
+    assert_eq!("z00", gates[&("XOR", BTreeSet::from(["x00", "y00"]))]);
     for i in 1..45 {
         let x = format!("x{i:02}");
         let y = format!("y{i:02}");
         let z = format!("z{i:02}");
-        println!("{x} {y} {z}");
-        let mut v1 = gates_reverse[&("XOR", BTreeSet::from([x.as_str(), y.as_str()]))];
-        if gates[z.as_str()] != ("XOR", BTreeSet::from([v1, carry])) {
-            let left = z.as_str();
-            let right = ("XOR", BTreeSet::from([v1, carry]));
-            println!("{x} {y} {z} {v1} {carry}");
-            if gates_reverse.contains_key(&right) {
-                let a = gates.remove(left).unwrap();
-                let b = gates_reverse.remove(&a).unwrap();
-                let c = gates_reverse.remove(&right).unwrap();
-                let d = gates.remove(c).unwrap();
-                result.push(c);
-                result.push(b);
-                gates.insert(c, a.clone());
-                gates.insert(b, d.clone());
-                gates_reverse.insert(a, c);
-                gates_reverse.insert(d, b);
-            } else {
-                let left = v1;
-                let right = *gates[z.as_str()].1.iter().find(|val| **val != carry).unwrap();
-                let a = gates.remove(left).unwrap();
-                let d = gates.remove(right).unwrap();
-                let b = gates_reverse.remove(&a).unwrap();
-                let c = gates_reverse.remove(&d).unwrap();
-                result.push(c);
-                result.push(b);
-                gates.insert(c, a.clone());
-                gates.insert(b, d.clone());
-                gates_reverse.insert(a, c);
-                gates_reverse.insert(d, b);
-                v1 = right;
+
+        let mut x_and_y = gates[&("AND", BTreeSet::from([x.as_str(), y.as_str()]))];
+
+        let mut x_xor_y = gates[&("XOR", BTreeSet::from([x.as_str(), y.as_str()]))];
+        if let Some(&swap_x_xor_y) = op_map.get(&BTreeSet::from([z.as_str(), "XOR", carry])) {
+            if x_xor_y != swap_x_xor_y {
+                result.extend([x_xor_y, swap_x_xor_y]);
+                if x_and_y == swap_x_xor_y {
+                    x_and_y = x_xor_y;
+                }
+                x_xor_y = swap_x_xor_y;
             }
+        } else if let Some(&z_swap) = gates.get(&("XOR", BTreeSet::from([x_xor_y, carry]))) {
+            if x_and_y == z {
+                x_and_y = z_swap;
+            }
+            if gates[&("AND", BTreeSet::from([x_xor_y, carry]))] == z {
+                gates.insert(("AND", BTreeSet::from([x_xor_y, carry])), z_swap);
+            }
+            let new_carry = gates[&("AND", BTreeSet::from([x_xor_y, carry]))];
+            if gates[&("OR", BTreeSet::from([x_and_y, new_carry]))] == z {
+                gates.insert(("OR", BTreeSet::from([x_and_y, new_carry])), z_swap);
+            }
+            result.extend([z_swap, &*z.leak()]);
+        } else {
+            result.push(carry);
+            result.push(op_map[&BTreeSet::from(["XOR", x_xor_y, z.as_str()])]);
         }
-        let new_carry = gates_reverse[&("AND", BTreeSet::from([x.as_str(), y.as_str()]))];
-        let v2 = gates_reverse[&("AND", BTreeSet::from([v1, carry]))];
 
-        println!("{x} {y} {z} {v1} {v2} {carry} {new_carry}");
-
-        if !gates_reverse.contains_key(&("OR", BTreeSet::from([new_carry, v2]))) {
-            let left = {
-                let vals = &gates_reverse
-                    .keys()
-                    .find(|(op, vals)| *op == "AND" && vals.contains(carry))
-                    .unwrap()
-                    .1;
-                *vals.iter().find(|val| **val != carry).unwrap()
-            };
-            let right = ("AND", BTreeSet::from([v1, carry]));
-            let a = gates.remove(left).unwrap();
-            let b = gates_reverse.remove(&a).unwrap();
-            let c = gates_reverse.remove(&right).unwrap();
-            let d = gates.remove(c).unwrap();
-            result.push(c);
-            result.push(b);
-            gates.insert(c, a.clone());
-            gates.insert(b, d.clone());
-            gates_reverse.insert(a, c);
-            gates_reverse.insert(d, b);
-        }
-        carry = gates_reverse[&("OR", BTreeSet::from([new_carry, v2]))];
+        let new_carry = gates[&("AND", BTreeSet::from([x_xor_y, carry]))];
+        carry = gates[&("OR", BTreeSet::from([x_and_y, new_carry]))];
     }
+    assert_eq!("z45", carry);
 
     result.sort_unstable();
     result.join(",")
