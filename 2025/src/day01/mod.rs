a8d32fd4358dc19169c85030b5e4e0da981b1df8
Tim Balsfulland <tim@balsfulland.email>
2025-11-30 17:47:37 +0100

    2025 preparation

---
+++ 2025/src/day01/mod.rs
@@ -0,0 +1,21 @@
+pub fn solve_first(input: &str) -> usize {
+	todo!()
+}
+
+pub fn solve_second(input: &str) -> usize {
+	todo!()
+}
+
+#[test]
+pub fn sample() {
+    let sample = include_str!("sample.txt");
+    assert_eq!(0, solve_first(sample));
+    assert_eq!(0, solve_second(sample));
+}
+
+#[test]
+pub fn input() {
+    let input = include_str!("input.txt");
+    assert_eq!(0, solve_first(input));
+    assert_eq!(0, solve_second(input));
+}
