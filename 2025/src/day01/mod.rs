95af133dc2e8089d4573388a8e6fb606c4f682bc
Tim Balsfulland <tim@balsfulland.email>
2025-12-01 18:24:56 +0100

    2025 Day 1

--- 2025/src/day01/mod.rs
+++ 2025/src/day01/mod.rs
@@ -1,21 +1,63 @@
 pub fn solve_first(input: &str) -> usize {
-	todo!()
+    let mut dial = 50;
+    let mut count = 0;
+
+    for line in input.lines() {
+        let (direction, amount) = line.split_at(1);
+
+        let amount: isize = amount.parse().unwrap();
+        let operation = match direction {
+            "L" => isize::strict_sub,
+            "R" => isize::strict_add,
+            _ => unreachable!(),
+        };
+
+        dial = operation(dial, amount).rem_euclid(100);
+
+        if dial == 0 {
+            count += 1;
+        }
+    }
+
+    count
 }
 
 pub fn solve_second(input: &str) -> usize {
-	todo!()
+    let mut dial = 50;
+    let mut count = 0;
+
+    for line in input.lines() {
+        let (direction, amount) = line.split_at(1);
+
+        let mut amount: isize = amount.parse().unwrap();
+        let operation = match direction {
+            "L" => isize::strict_sub,
+            "R" => isize::strict_add,
+            _ => unreachable!(),
+        };
+
+        while amount > 0 {
+            amount -= 1;
+            dial = operation(dial, 1).rem_euclid(100);
+            if dial == 0 {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
 
 #[test]
 pub fn sample() {
     let sample = include_str!("sample.txt");
-    assert_eq!(0, solve_first(sample));
-    assert_eq!(0, solve_second(sample));
+    assert_eq!(3, solve_first(sample));
+    assert_eq!(6, solve_second(sample));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(0, solve_first(input));
-    assert_eq!(0, solve_second(input));
+    assert_eq!(1165, solve_first(input));
+    assert_eq!(6496, solve_second(input));
 }
