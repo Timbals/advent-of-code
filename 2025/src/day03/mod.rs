0c0bde2ae994b5e9922e7de4ddca6efd9950f3e2
Tim Balsfulland <tim@balsfulland.email>
2025-12-03 15:28:31 +0100

    simplify 2025 Day 3

--- 2025/src/day03/mod.rs
+++ 2025/src/day03/mod.rs
@@ -1,29 +1,14 @@
-use itertools::Itertools;
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
 
-pub fn solve_first(input: &str) -> usize {
-    let mut joltage = 0;
-
-    for bank in input.lines().map(str::as_bytes) {
-        let first = bank[..bank.len() - 1].iter().position_min_by_key(|x| b'9' - **x).unwrap();
-        let second =
-            bank[first + 1..].iter().position_min_by_key(|x| b'9' - **x).unwrap() + first + 1;
-        let (first, second) = (bank[first] - b'0', bank[second] - b'0');
-        joltage += first as usize * 10 + second as usize;
-    }
-
-    joltage
-}
-
-pub fn solve_second(input: &str) -> usize {
+pub fn solve(input: &str, k: usize) -> usize {
     let mut total = 0;
 
     for bank in input.lines().map(str::as_bytes) {
         let mut joltage = 0;
         let mut remaining = bank.iter().enumerate().map(|(i, x)| (*x, Reverse(i)));
-        let mut heap = BinaryHeap::from_iter(remaining.by_ref().take(bank.len() - 12));
-        for _ in 0..12 {
+        let mut heap = BinaryHeap::from_iter(remaining.by_ref().take(bank.len() - k));
+        for _ in 0..k {
             heap.push(remaining.next().unwrap());
             let (x, Reverse(i)) = heap.pop().unwrap();
             heap.retain(|(_, Reverse(pos))| *pos > i);
@@ -38,13 +23,13 @@ pub fn solve_second(input: &str) -> usize {
 #[test]
 pub fn sample() {
     let sample = include_str!("sample.txt");
-    assert_eq!(357, solve_first(sample));
-    assert_eq!(3121910778619, solve_second(sample));
+    assert_eq!(357, solve(sample, 2));
+    assert_eq!(3121910778619, solve(sample, 12));
 }
 
 #[test]
 pub fn input() {
     let input = include_str!("input.txt");
-    assert_eq!(17034, solve_first(input));
-    assert_eq!(168798209663590, solve_second(input));
+    assert_eq!(17034, solve(input, 2));
+    assert_eq!(168798209663590, solve(input, 12));
 }
