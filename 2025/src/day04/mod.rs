02ab140df0e3b1101fdb11e92f7a161f40359ece
Tim Balsfulland <tim@balsfulland.email>
2025-12-04 11:53:00 +0100

    simplify 2025 Day 4

--- 2025/src/day04/mod.rs
+++ 2025/src/day04/mod.rs
@@ -1,43 +1,25 @@
+use itertools::Itertools;
+
 pub fn solve_first(input: &str) -> usize {
     let grid = input.lines().map(|line| line.as_bytes()).collect::<Vec<_>>();
 
     let mut total = 0;
 
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            if grid[y][x] != b'@' {
-                continue;
+    for (y, x) in Itertools::cartesian_product(0..grid.len(), 0..grid[0].len()) {
+        if grid[y][x] != b'@' {
+            continue;
+        }
+
+        let mut neighbors = 0;
+        for (dy, dx) in Itertools::cartesian_product(-1..=1, -1..=1) {
+            if let Some(row) = grid.get(y.wrapping_add_signed(dy))
+                && row.get(x.wrapping_add_signed(dx)) == Some(&b'@')
+            {
+                neighbors += 1;
             }
-
-            let mut neighbors = 0;
-            for dy in -1..=1 {
-                let Some(y) = y.checked_add_signed(dy) else {
-                    continue;
-                };
-                if y >= grid.len() {
-                    continue;
-                }
-
-                for dx in -1..=1 {
-                    if dy == 0 && dx == 0 {
-                        continue;
-                    }
-
-                    let Some(x) = x.checked_add_signed(dx) else {
-                        continue;
-                    };
-                    if x >= grid[0].len() {
-                        continue;
-                    }
-
-                    if grid[y][x] == b'@' {
-                        neighbors += 1;
-                    }
-                }
-            }
-            if neighbors < 4 {
-                total += 1;
-            }
+        }
+        if neighbors <= 4 {
+            total += 1;
         }
     }
 
@@ -52,43 +34,23 @@ pub fn solve_second(input: &str) -> usize {
     let mut done = false;
     while !done {
         done = true;
-        for y in 0..grid.len() {
-            for x in 0..grid[0].len() {
-                if grid[y][x] != b'@' {
-                    continue;
+        for (y, x) in Itertools::cartesian_product(0..grid.len(), 0..grid[0].len()) {
+            if grid[y][x] != b'@' {
+                continue;
+            }
+
+            let mut neighbors = 0;
+            for (dy, dx) in Itertools::cartesian_product(-1..=1, -1..=1) {
+                if let Some(row) = grid.get(y.wrapping_add_signed(dy))
+                    && row.get(x.wrapping_add_signed(dx)) == Some(&b'@')
+                {
+                    neighbors += 1;
                 }
-
-                let mut neighbors = 0;
-                for dy in -1..=1 {
-                    let Some(y) = y.checked_add_signed(dy) else {
-                        continue;
-                    };
-                    if y >= grid.len() {
-                        continue;
-                    }
-
-                    for dx in -1..=1 {
-                        if dy == 0 && dx == 0 {
-                            continue;
-                        }
-
-                        let Some(x) = x.checked_add_signed(dx) else {
-                            continue;
-                        };
-                        if x >= grid[0].len() {
-                            continue;
-                        }
-
-                        if grid[y][x] == b'@' {
-                            neighbors += 1;
-                        }
-                    }
-                }
-                if neighbors < 4 {
-                    total += 1;
-                    grid[y][x] = b'.';
-                    done = false;
-                }
+            }
+            if neighbors <= 4 {
+                total += 1;
+                grid[y][x] = b'.';
+                done = false;
             }
         }
     }
