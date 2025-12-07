2fbd00c8cd234c65823323c9f68c7f45fd1b34f8
Tim Balsfulland <tim@balsfulland.email>
2025-12-07 17:26:16 +0100

    simplify 2025 Day 7

--- 2025/src/day07/mod.rs
+++ 2025/src/day07/mod.rs
@@ -1,44 +1,35 @@
 pub fn solve_first(input: &str) -> usize {
-    let mut grid = input.lines().map(|line| line.as_bytes().to_vec()).collect::<Vec<_>>();
-    let start = grid
-        .iter()
-        .enumerate()
-        .find_map(|(y, line)| {
-            line.iter().enumerate().find(|(_, byte)| **byte == b'S').map(|(x, _)| (x, y))
-        })
-        .unwrap();
-
-    let mut stack = vec![start];
+    let width = input.lines().next().unwrap().len();
+    let mut state = vec![false; width];
+    let mut next_state = vec![false; width];
     let mut split_count = 0;
-
-    while let Some((x, y)) = stack.pop() {
-        match grid.get(y).and_then(|line| line.get(x)) {
-            Some(b'S') => {
-                stack.push((x, y + 1));
+    for row in input.lines().map(str::as_bytes) {
+        for (x, byte) in row.iter().copied().enumerate() {
+            match byte {
+                b'S' => next_state[x] = true,
+                b'.' => next_state[x] |= state[x],
+                b'^' => {
+                    if state[x] {
+                        split_count += 1;
+                    }
+                    next_state[x - 1] |= state[x];
+                    next_state[x + 1] |= state[x];
+                }
+                _ => unreachable!(),
             }
-            Some(b'.') => {
-                grid[y][x] = b'|';
-                stack.push((x, y + 1));
-            }
-            Some(b'^') => {
-                split_count += 1;
-                stack.push((x - 1, y));
-                stack.push((x + 1, y));
-            }
-            Some(b'|') | None => {}
-            _ => unreachable!(),
         }
+        std::mem::swap(&mut state, &mut next_state);
+        next_state.fill(false);
     }
-
     split_count
 }
 
 pub fn solve_second(input: &str) -> usize {
-    let grid = input.lines().map(str::as_bytes).collect::<Vec<_>>();
-    let mut state = vec![0; grid[0].len()];
-    let mut next_state = vec![0; grid[0].len()];
-    for line in grid.into_iter() {
-        for (x, byte) in line.iter().copied().enumerate() {
+    let width = input.lines().next().unwrap().len();
+    let mut state = vec![0; width];
+    let mut next_state = vec![0; width];
+    for row in input.lines().map(str::as_bytes) {
+        for (x, byte) in row.iter().copied().enumerate() {
             match byte {
                 b'S' => next_state[x] += 1,
                 b'.' => next_state[x] += state[x],
@@ -52,7 +43,6 @@ pub fn solve_second(input: &str) -> usize {
         std::mem::swap(&mut state, &mut next_state);
         next_state.fill(0);
     }
-
     state.into_iter().sum()
 }
 
