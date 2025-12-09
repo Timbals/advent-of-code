f6c7c3049fca9df4c3839a3dac534b2251bc9b00
Tim Balsfulland <tim@balsfulland.email>
2025-12-09 16:39:08 +0100

    simplify 2025 Day 9

--- 2025/src/day09/mod.rs
+++ 2025/src/day09/mod.rs
@@ -1,14 +1,18 @@
 use itertools::Itertools;
 use std::cmp::{max, min};
 
-pub fn solve_first(input: &str) -> usize {
-    let red_tiles = input
+fn parse(input: &str) -> Vec<(usize, usize)> {
+    input
         .lines()
         .map(|line| {
             let (x, y) = line.split_once(',').unwrap();
             (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
         })
-        .collect::<Vec<_>>();
+        .collect()
+}
+
+pub fn solve_first(input: &str) -> usize {
+    let red_tiles = parse(input);
 
     red_tiles
         .into_iter()
@@ -19,31 +23,26 @@ pub fn solve_first(input: &str) -> usize {
 }
 
 pub fn solve_second(input: &str) -> usize {
-    let red_tiles = input
-        .lines()
-        .map(|line| {
-            let (x, y) = line.split_once(',').unwrap();
-            (x.parse::<usize>().unwrap(), y.parse::<usize>().unwrap())
-        })
-        .collect::<Vec<_>>();
+    let red_tiles = parse(input);
 
     red_tiles
         .iter()
-        .copied()
         .tuple_combinations()
-        .filter(|&((x1, y1), (x2, y2))| {
+        .filter(|&(&(x1, y1), &(x2, y2))| {
+            // technically need to check this isn't like ((9, 7), (2, 5)) in the sample input
             let (x_min, x_max) = (min(x1, x2), max(x1, x2));
             let (y_min, y_max) = (min(y1, y2), max(y1, y2));
 
             red_tiles.iter().circular_tuple_windows().all(|(&(x3, y3), &(x4, y4))| {
-                // check that no edges are inside the rectangle
+                // check that no edges are inside the rectangle,
+                // technically not sufficient because it doesn't account for "snaking" edges
                 (x3 <= x_min && x4 <= x_min)
                     || (x3 >= x_max && x4 >= x_max)
                     || (y3 <= y_min && y4 <= y_min)
                     || (y3 >= y_max && y4 >= y_max)
             })
         })
-        .map(|((x1, y1), (x2, y2))| (x1.abs_diff(x2) + 1) * (y1.abs_diff(y2) + 1))
+        .map(|(&(x1, y1), &(x2, y2))| (x1.abs_diff(x2) + 1) * (y1.abs_diff(y2) + 1))
         .max()
         .unwrap()
 }
